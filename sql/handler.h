b3b56b2f03258160be24f82d7ddf580b5b7ed2d8
George Lorch <glorch@localhost.localdomain>
2012-05-14 14:38:47 -0700

    PS-5006: Port storage engine enforcement to 8.0
    1. The patch is based on https://github.com/percona/percona-server/commit/7ad12e4df3880ac11fbf7656a14bf9b8043d25e1 with improvements at https://github.com/percona/percona-server/commit/868dbf4ba27ac4974b98f11c835ae367bdeb0022 and https://github.com/percona/percona-server/commit/1b93144cd839fcb804e6c0fbe8bb4a30de27dce5
    2. Added exceptions for transactional data dictionary (DD) tables (enforcement is forbidden)
    3. `percona_enforce_se_create_table_like` was changed as `mysql.proc` was removed in 8.0
    4. Test `ALTER TABLE with ALGORITHM=INSTANT,INPLACE,COPY` in percona_enforce_storage_engine_alter_table.inc

--- sql/handler.h
+++ sql/handler.h
@@ -7108,6 +7108,7 @@ class DsMrr_impl {
 /* lookups */
 handlerton *ha_default_handlerton(THD *thd);
 handlerton *ha_default_temp_handlerton(THD *thd);
+handlerton *ha_enforce_handlerton(THD *thd);
 /**
   Resolve handlerton plugin by name, without checking for "DEFAULT" or
   HTON_NOT_USER_SELECTABLE.
