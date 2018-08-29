299061c6b1de34a14ba861b4afe61c35a6ee0853
Vlad Lesin <vlad_lesin@mail.ru>
2018-08-29 20:10:39 +0300

    PS-3890: Native Partitioning for TokuDB and RocksDB for 8.0
    partition_base.* are copied from ha_partition.* in 5.7 with corresponding fixes.
    
    5.7 required some additional code to read partition info from .frm files, in
    8.0 this functionality is not required, the corresponding code was removed.
    
    Currently "inline alter partition" and "exchange partition" are not implemented.
    
    As "inline alter partition" is not implemented, all "alter partition" operations
    involve full table copying. There can be such errors as "no partition found"
    duriring copying, which must be ignored, what is also implemented in the server
    code.
    
    The warning is added on alter partitioning to notify that inplace partition
    altering is not supported.
    
    The following test results were rewritten due to new warning:
    
    rocksdb      partition
    tokudb.bugs  alter_part_tokudb_bug_155
    tokudb.parts partition_mgm_lc10_tokudb partition_special_tokudb
                 partition_alter3_tokudb part_supported_sql_func_tokudb
    
    Testcase for http://bugs.mysql.com/bug.php?id=71270 was ported from 5.7. See
    bug71270.* files.

--- sql/handler.h
+++ sql/handler.h
@@ -1448,6 +1448,7 @@ typedef int (*alter_tablespace_t)(handlerton *hton, THD *thd,
 using flush_changed_page_bitmaps_t = bool (*)(void);
 
 using purge_changed_page_bitmaps_t = bool (*)(ulonglong lsn);
+
 /**
   SE interface for getting tablespace extension.
   @return Extension of tablespace datafile name.
@@ -5020,6 +5021,10 @@ class handler {
   */
 
   virtual bool is_ignorable_error(int error);
+  MY_NODISCARD virtual bool continue_partition_copying_on_error(
+      int error [[maybe_unused]]) {
+    return false;
+  }
 
   /**
     @brief Determine whether an error is fatal or not.
