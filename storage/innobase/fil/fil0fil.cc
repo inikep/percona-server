af40f0ee206ea1d1c12f26700526cac71656d9fa
Satya Bodapati <satya.bodapati@percona.com>
2018-09-20 14:45:42 +0530

    [encryption] PS-4834 - encrypted system tablespace has empty uuid
    Problem:
    -------
    
    This is happening because when master key is created at innodb
    initialization time, it gets server uuid. But server generates
    server_uuid() only after innodb intialization is done.
    So an empty uuid is given to InnoDB by server.
    
    Since master key is created with empty uuid and all tables will
    continue this.
    
    Whats the fix then?:
    
    Can't we not make server generate UUID before innodb is intialized.
    Well, not that simple. the UUID generation code needs THD, which
    inturn needs all plugins to be initialized. So it is kind of
    chicken and egg problem. (InnoDB needs server_uuid() to be ready
    and server_uuid() needs InnoDB to ready)
    
    Ideally server uuid generation shouldn't have any dependencies with InnoDB
    (the best fix but hard)
    
    How upstream avoids empty UUID with tablespaces like redo(in 8.0):
    
    1. Like system, redo is also created with empty uuid, default masterkey
    2. Background threads keep checking for the default master key id and then
       tries to rotate the encryption info of a tablespace
    
    Fix:
    ----
    Unlike upstream, we will use a new handlerton method. This is a better
    way because
    
    1. It doesn't rely on background threads to fix empty UUID
    2. Allows redo and undo encryption to be enabled from foreground threads.
       As part of variable update function
    3. Fixes empty UUID for tablespaces not only at bootstrap but also for
       tablespaces that are encrypted at startup.
    4. Fixes empty UUID not only innodb global tablespaces(system, temp etc),
       but also for all mysql.* ibds. They can be chosen to encrypted if
       --innodb-encrypt-tables is ON during bootstrap
    
    Added debug asserts that crash when empty UUID is written to
    tablespace header and also crash if empty UUID is read from tablespace
    header.
    (cherry picked from commit 30330d0f831f65d3918486d492f388aaeeb63598)
    
    ---------------------------------------------------------------------------
    
    PS-4917 : Assertion failure: dict0dd.cc:5784:space->flags == flags
    
    Problem:
    --------
    When innodb system tablespace is encrypted, on crash recovery,
    the system tablespace flags are mismatching. i.e the flags from
    data dictionary are mismatching with the flags from ibdata1 file.
    
    Fix:
    ----
    Store correct InnoDB system tablespace flags in DD

--- storage/innobase/fil/fil0fil.cc
+++ storage/innobase/fil/fil0fil.cc
@@ -9505,6 +9505,32 @@ dberr_t fil_temp_update_encryption(fil_space_t *space) {
   return (err);
 }
 
+/** Rotate the tablespace key by new master key.
+@param[in]	space	tablespace object
+@return true if the re-encrypt suceeds */
+static bool encryption_rotate_low(fil_space_t *space) {
+  bool success = true;
+  if (space->encryption_type != Encryption::NONE) {
+    mtr_t mtr;
+    mtr_start(&mtr);
+
+    if (fsp_is_system_temporary(space->id)) {
+      mtr_set_log_mode(&mtr, MTR_LOG_NO_REDO);
+    }
+
+    mtr_x_lock_space(space, &mtr);
+
+    byte encrypt_info[Encryption::INFO_SIZE];
+    memset(encrypt_info, 0, Encryption::INFO_SIZE);
+
+    if (!fsp_header_rotate_encryption(space, encrypt_info, &mtr)) {
+      success = false;
+    }
+    mtr_commit(&mtr);
+  }
+  return (success);
+}
+
 #ifndef UNIV_HOTBACKUP
 bool Fil_shard::needs_encryption_rotate(fil_space_t *space) {
   /* We only rotate if encryption is already set. */
@@ -9515,6 +9541,12 @@ bool Fil_shard::needs_encryption_rotate(fil_space_t *space) {
   /* Deleted spaces do not need rotation.  Their pages are being
   deleted from the buffer pool. */
   if (space->is_deleted()) {
+    return false;
+  }
+
+  /* Skip if space is not master encrypted */
+
+  if (space->encryption_type != Encryption::AES) {
     return false;
   }
 
@@ -9529,7 +9561,6 @@ bool Fil_shard::needs_encryption_rotate(fil_space_t *space) {
       "ib_encryption_rotate_skip",
       ib::info(ER_IB_MSG_INJECT_FAILURE, "ib_encryption_rotate_skip");
       return false;);
-
   return true;
 }
 
@@ -9612,6 +9643,21 @@ size_t Fil_system::encryption_rotate() {
 }
 
 size_t fil_encryption_rotate() { return (fil_system->encryption_rotate()); }
+
+bool fil_encryption_rotate_global(const space_id_vec &space_ids) {
+  for (space_id_t space_id : space_ids) {
+    fil_space_t *space = fil_space_acquire(space_id);
+
+    bool success = encryption_rotate_low(space);
+
+    fil_space_release(space);
+
+    if (!success) {
+      return (false);
+    }
+  }
+  return (true);
+}
 
 #endif /* !UNIV_HOTBACKUP */
 
