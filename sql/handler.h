3771cbc383cf192e0dff22e6a5edc5a2435f9ff1
Satya Bodapati <satya.bodapati@percona.com>
2018-09-20 14:45:42 +0530

    PS-4834 - encrypted system tablespace has empty uuid
    Problem:
    -------
    
    This is happening because when master key is created at innodb
    initialization time, it gets server uuid. But server generates
    server_uuid() only after innodb intialization is done.
    So an empty uuid is given to InnoDB by server.
    
    Since master key is created with empty uuid and all tables will
    continue this.
    
    Whats the fix then?:
    
    Can't we not make server generate UUID before innodb is intialized.
    Well, not that simple. the UUID generation code needs THD, which
    inturn needs all plugins to be initialized. So it is kind of
    chicken and egg problem. (InnoDB needs server_uuid() to be ready
    and server_uuid() needs InnoDB to ready)
    
    Ideally server uuid generation shouldn't have any dependencies with InnoDB
    (the best fix but hard)
    
    How upstream avoids empty UUID with tablespaces like redo(in 8.0):
    
    1. Like system, redo is also created with empty uuid, default masterkey
    2. Background threads keep checking for the default master key id and then
       tries to rotate the encryption info of a tablespace
    
    Fix:
    ----
    Unlike upstream, we will use a new handlerton method. This is a better
    way because
    
    1. It doesn't rely on background threads to fix empty UUID
    2. Allows redo and undo encryption to be enabled from foreground threads.
       As part of variable update function
    3. Fixes empty UUID for tablespaces not only at bootstrap but also for
       tablespaces that are encrypted at startup.
    4. Fixes empty UUID not only innodb global tablespaces(system, temp etc),
       but also for all mysql.* ibds. They can be chosen to encrypted if
       --innodb-encrypt-tables is ON during bootstrap
    
    Added debug asserts that crash when empty UUID is written to
    tablespace header and also crash if empty UUID is read from tablespace
    header.
    (cherry picked from commit 30330d0f831f65d3918486d492f388aaeeb63598)
    
    ---------------------------------------------------------------------------
    
    PS-4917 : Assertion failure: dict0dd.cc:5784:space->flags == flags
    
    Problem:
    --------
    When innodb system tablespace is encrypted, on crash recovery,
    the system tablespace flags are mismatching. i.e the flags from
    data dictionary are mismatching with the flags from ibdata1 file.
    
    Fix:
    ----
    Store correct InnoDB system tablespace flags in DD

--- sql/handler.h
+++ sql/handler.h
@@ -2200,6 +2200,7 @@ struct handlerton {
   notify_exclusive_mdl_t notify_exclusive_mdl;
   notify_alter_table_t notify_alter_table;
   rotate_encryption_master_key_t rotate_encryption_master_key;
+  fix_tablespaces_empty_uuid_t fix_tablespaces_empty_uuid;
   upgrade_get_compression_dict_data_t upgrade_get_compression_dict_data;
 
   get_table_statistics_t get_table_statistics;
