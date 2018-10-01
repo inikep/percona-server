8c0516112791184f2ffdfd24cd6a19d4718d5bb7
przemyslaw.skibinski@percona.com <przemyslaw.skibinski@percona.com>
2018-10-01 15:02:09 +0200

    PS-4805: Re-implement compression dictionaries in 8.0 - v1
    1.  Re-implement CREATE COMPRESSION_DICTIONARY, DROP COMPRESSION_DICTIONARY
    2.  Allow CREATE TABLE to use 'WITH COMPRESSION_DICTIONARY'
    3.  DDLs on tables with compression dictionary
    4.  Prevent dropping of COMPRESSION DICTIONARY with existing references
    5.  Allow compression_dictionary on tables with partitions
    6.  Fix mysqldump to show compression dictionary
    7.  New I_S views on compression dictionary tables
    8.  Fix tests to use new I_S views
    9.  Throw proper errors (ER_COMPRESSION_*)
    10. Error handling in innodb_read_only mode
    11. Handle upgrade from 5.7
    12. Handle creation of dictionary tables in 8.0 at startup (mysql-8.0 -> PS8.0 )
    13. Add upgrade test
    14. Prevent direct access to mysql.compression_dictionary and mysql.compression_dictionary_cols table
    
    Fix PS-5140 (Out-of-bounds reads for compression dictionary object names)
    
    Fix out-of-bounds reads of sql_zip_dict.cc:COMPRESSION_DICTIONARY_DB,
    COMPRESSION_DICTIONARY_TABLE, COMPRESSION_DICTIONARY_COLS_DB, and
    COMPRESSION_DICTIONARY_COLS_TABLE caused by incorrect their constexpr
    declarations, resulting in these constants being treated as
    pointer (sizeof == 8 bytes) rather than string literals.
    
    At the same time replace TALBE_LIST::init_one_table uses with
    TABLE_LIST::TABLE_LIST, as recommended for new code.
    
    At the same time audit other Percona-added constexprs for the same
    issue and adjust log0online.cc:bmp_file_name_stem and
    bmp_file_name_template accordingly, even though their sizeof is never
    taken, hence the issue is currently benign for them.
    
    Fix PS-5146 (UBSan: strncmp(nullptr, ... ) called by / is_equal / Field::has_different_compression_attributes_with)
    
    Avoid calling strncmp with nullptr in is_equal(const LEX_CSTRING *,
    const LEX_CSTRING *) by rewriting
    Field::has_different_compression_attributes_with to assume that the
    compression attributes are the same if both zip_dict_name.str ==
    nullptr. At the same time rewrite this method to break down a complex
    bool conditional.
    
    Fix PS-5311 (create_zip_dict: debug output may access past string end)
    
    In create_zip_dict and drop_zip_dict, name and data args are pointers
    to char arrays whose length is determined by other args rathen by
    terminating NULs. Thus, passing them as-is to operator<< in DBUG_LOG
    results in reads past array end. Fix by creating temp std::string
    objects using the passed lengths, and passing that to operator<<.
    
    Fix PS-5484 (mysql.json std_data files have wrong char_length for mysql.compression_dictionary.data)
    
    The affected field mysql.compression_dictionary_data is of type BLOB,
    whose char length is 65535, as the test tries to check. Update the
    reference files in std_data.
    
    Fix PS-5456 (Empty xtradb_zip_dict MTR testcase check not restored in 8.0)
    
    Restore the MTR testcase check that was supposed to be disabled only
    temporarily, using the new 8.0 name of the
    table (INFORMATION_SCHEMA.COMPRESSION_DICTIONARY). At the same time
    clean up old XTRADB_ZIP_DICT and XTRADB_ZIP_DICT_COLS references in
    the tests.
    
    PS-5363 (Merge MySQL 8.0.17): Fix Compressed column test failures
    
    The problem is that 5.7 uses old value of DATA_COMPRESSED in dictionary and 8.0 uses newer
    value
    
    Fix:
    ----
    Uses 5.7 DATA_COMPRESSED to compare the precision types during upgrade.
    
    Fix PS-5270 (xtradb_compressed_columns_consistency_barracuda_compressed_set2 still too big under ASan)
    
    mysqltest keeps in memory all the processed lines, with loops
    unrolled. For a testcase with multiple nested loops this results in
    big memory usage. Fix by rewriting the testcase logic as server-side
    stored procedures, completely eliminating mysqltest memory concerns,
    and making the testcase more efficient overall.
    
    PS-5674: Fixing i_s_schema_definition_debug
    
    Testcase wasn't aware of compression dictionaries.

--- storage/innobase/include/row0mysql.h
+++ storage/innobase/include/row0mysql.h
@@ -104,6 +104,31 @@ void row_mysql_prebuilt_free_blob_heap(
 */
 void row_mysql_prebuilt_free_compress_heap(row_prebuilt_t *prebuilt) noexcept;
 
+/** Uncompress blob/text/varchar column using zlib
+@param[in]  data    data in InnoDB (compressed) format
+@param[in,out]  len in: data length, out: length of decomprssed data
+@param[in]  dict_data   optional dictionary data used for decompression
+@param[in]  dict_data_len   optional dictionary data length
+@param[in]  prebuilt    use prebuilt->compress_heap only here
+@return pointer to the uncompressed data */
+MY_NODISCARD
+const byte *row_decompress_column(const byte *data, ulint *len,
+                                  const byte *dict_data, ulint dict_data_len,
+                                  row_prebuilt_t *prebuilt);
+
+/** Compress blob/text/varchar column using zlib
+@param[in]  data    data in MySQL (uncompressed) format
+@param[in,out]  len in: data length: out: length of compressed data
+@param[in]  lenlen  bytes used to store the length of data
+@param[in]  dict_data   optional dictionary data used for compression
+@param[in]  dict_data_len   optional dictionary data length
+@param[in]  prebuilt    use prebuilt->compress_heap only
+@return pointer to the compressed data */
+MY_NODISCARD
+byte *row_compress_column(const byte *data, ulint *len, ulint lenlen,
+                          const byte *dict_data, ulint dict_data_len,
+                          row_prebuilt_t *prebuilt);
+
 /** Stores a >= 5.0.3 format true VARCHAR length to dest, in the MySQL row
  format.
  @return pointer to the data, we skip the 1 or 2 bytes at the start
@@ -130,16 +155,34 @@ void row_mysql_store_blob_ref(
                       to 4 bytes */
     const void *data, /*!< in: BLOB data; if the value to store
                       is SQL NULL this should be NULL pointer */
-    ulint len);       /*!< in: BLOB length; if the value to store
-                      is SQL NULL this should be 0; remember
-                      also to set the NULL bit in the MySQL record
-                      header! */
+    ulint len,        /*!< in: BLOB length; if the value to store
+            is SQL NULL this should be 0; remember
+            also to set the NULL bit in the MySQL record
+            header! */
+    bool need_decompression,
+    /*!< in: if the data need to be compressed*/
+    const byte *dict_data,
+    /*!< in: optional compression dictionary
+    data */
+    ulint dict_data_len,
+    /*!< in: optional compression dictionary data
+    length */
+    row_prebuilt_t *prebuilt);
+/*<! in: use prebuilt->compress_heap only
+here */
 /** Reads a reference to a BLOB in the MySQL format.
 @param[out] len                 BLOB length.
 @param[in] ref                  BLOB reference in the MySQL format.
 @param[in] col_len              BLOB reference length (not BLOB length).
+@param[in] need_compression     if the data need to be compressed
+@param[in] dict_data            optional compression dictionary data
+@param[in] dict_data_len        optional compression dictionary data length
+@param[in] prebuilt             use prebuilt->compress_heap only heap
 @return pointer to BLOB data */
-const byte *row_mysql_read_blob_ref(ulint *len, const byte *ref, ulint col_len);
+const byte *row_mysql_read_blob_ref(ulint *len, const byte *ref, ulint col_len,
+                                    bool need_compression,
+                                    const byte *dict_data, ulint dict_data_len,
+                                    row_prebuilt_t *prebuilt);
 
 /** Converts InnoDB geometry data format to MySQL data format. */
 void row_mysql_store_geometry(
@@ -191,7 +234,16 @@ byte *row_mysql_store_col_in_innobase_format(
                             necessarily the length of the actual
                             payload data; if the column is a true
                             VARCHAR then this is irrelevant */
-    ulint comp);            /*!< in: nonzero=compact format */
+    ulint comp,             /*!< in: nonzero=compact format */
+    bool need_compression,
+    /*!< in: if the data need to be
+    compressed */
+    const byte *dict_data,     /*!< in: optional compression
+                               dictionary data */
+    ulint dict_data_len,       /*!< in: optional compression
+                               dictionary data length */
+    row_prebuilt_t *prebuilt); /*!< in: use prebuilt->compress_heap
+                               only here */
 /** Handles user errors and lock waits detected by the database engine.
  @return true if it was a lock wait and we should continue running the
  query thread */
@@ -549,7 +601,9 @@ struct mysql_row_templ_t {
                                 it is an unsigned integer type */
   ulint is_virtual;             /*!< if a column is a virtual column */
   ulint is_multi_val;           /*!< if a column is a Multi-Value Array virtual
-                                column */
+                                  column */
+  bool compressed;              /*!< if column format is compressed */
+  LEX_CSTRING zip_dict_data;    /*!< associated compression dictionary */
 };
 
 #define MYSQL_FETCH_CACHE_SIZE 8
