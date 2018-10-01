2c4f89898cea19e52164c5ca9d2e1276a2001825
przemyslaw.skibinski@percona.com <przemyslaw.skibinski@percona.com>
2018-10-01 15:02:09 +0200

    [compression] PS-4805: Re-implement compression dictionaries in 8.0
    1.  Re-implement CREATE COMPRESSION_DICTIONARY, DROP COMPRESSION_DICTIONARY
    2.  Allow CREATE TABLE to use 'WITH COMPRESSION_DICTIONARY'
    3.  DDLs on tables with compression dictionary
    4.  Prevent dropping of COMPRESSION DICTIONARY with existing references
    5.  Allow compression_dictionary on tables with partitions
    6.  Fix mysqldump to show compression dictionary
    7.  New I_S views on compression dictionary tables
    8.  Fix tests to use new I_S views
    9.  Throw proper errors (ER_COMPRESSION_*)
    10. Error handling in innodb_read_only mode
    11. Handle upgrade from 5.7
    12. Handle creation of dictionary tables in 8.0 at startup (mysql-8.0 -> PS8.0 )
    13. Add upgrade test
    14. Prevent direct access to mysql.compression_dictionary and mysql.compression_dictionary_cols table
    
    Fix PS-5140 (Out-of-bounds reads for compression dictionary object names)
    
    Fix out-of-bounds reads of sql_zip_dict.cc:COMPRESSION_DICTIONARY_DB,
    COMPRESSION_DICTIONARY_TABLE, COMPRESSION_DICTIONARY_COLS_DB, and
    COMPRESSION_DICTIONARY_COLS_TABLE caused by incorrect their constexpr
    declarations, resulting in these constants being treated as
    pointer (sizeof == 8 bytes) rather than string literals.
    
    At the same time replace TALBE_LIST::init_one_table uses with
    TABLE_LIST::TABLE_LIST, as recommended for new code.
    
    At the same time audit other Percona-added constexprs for the same
    issue and adjust log0online.cc:bmp_file_name_stem and
    bmp_file_name_template accordingly, even though their sizeof is never
    taken, hence the issue is currently benign for them.
    
    Fix PS-5146 (UBSan: strncmp(nullptr, ... ) called by / is_equal / Field::has_different_compression_attributes_with)
    
    Avoid calling strncmp with nullptr in is_equal(const LEX_CSTRING *,
    const LEX_CSTRING *) by rewriting
    Field::has_different_compression_attributes_with to assume that the
    compression attributes are the same if both zip_dict_name.str ==
    nullptr. At the same time rewrite this method to break down a complex
    bool conditional.
    
    Fix PS-5311 (create_zip_dict: debug output may access past string end)
    
    In create_zip_dict and drop_zip_dict, name and data args are pointers
    to char arrays whose length is determined by other args rathen by
    terminating NULs. Thus, passing them as-is to operator<< in DBUG_LOG
    results in reads past array end. Fix by creating temp std::string
    objects using the passed lengths, and passing that to operator<<.
    
    Fix PS-5484 (mysql.json std_data files have wrong char_length for mysql.compression_dictionary.data)
    
    The affected field mysql.compression_dictionary_data is of type BLOB,
    whose char length is 65535, as the test tries to check. Update the
    reference files in std_data.
    
    Fix PS-5456 (Empty xtradb_zip_dict MTR testcase check not restored in 8.0)
    
    Restore the MTR testcase check that was supposed to be disabled only
    temporarily, using the new 8.0 name of the
    table (INFORMATION_SCHEMA.COMPRESSION_DICTIONARY). At the same time
    clean up old XTRADB_ZIP_DICT and XTRADB_ZIP_DICT_COLS references in
    the tests.
    
    PS-5363 (Merge MySQL 8.0.17): Fix Compressed column test failures
    
    The problem is that 5.7 uses old value of DATA_COMPRESSED in dictionary and 8.0 uses newer
    value
    
    Fix:
    ----
    Uses 5.7 DATA_COMPRESSED to compare the precision types during upgrade.
    
    Fix PS-5270 (xtradb_compressed_columns_consistency_barracuda_compressed_set2 still too big under ASan)
    
    mysqltest keeps in memory all the processed lines, with loops
    unrolled. For a testcase with multiple nested loops this results in
    big memory usage. Fix by rewriting the testcase logic as server-side
    stored procedures, completely eliminating mysqltest memory concerns,
    and making the testcase more efficient overall.
    
    PS-5674: Fixing i_s_schema_definition_debug
    
    Testcase wasn't aware of compression dictionaries.

--- sql/dd/impl/system_registry.cc
+++ sql/dd/impl/system_registry.cc
@@ -27,9 +27,11 @@
 #include "sql/dd/impl/system_views/character_sets.h"     // Character_sets
 #include "sql/dd/impl/system_views/check_constraints.h"  // Check_constraints
 #include "sql/dd/impl/system_views/collation_charset_applicability.h"  // Collati...
-#include "sql/dd/impl/system_views/collations.h"           // Collations
-#include "sql/dd/impl/system_views/column_statistics.h"    // Column_statistics
-#include "sql/dd/impl/system_views/columns.h"              // Columns
+#include "sql/dd/impl/system_views/collations.h"         // Collations
+#include "sql/dd/impl/system_views/column_statistics.h"  // Column_statistics
+#include "sql/dd/impl/system_views/columns.h"            // Columns
+#include "sql/dd/impl/system_views/compression_dictionary.h"  // Compression_dictionary
+#include "sql/dd/impl/system_views/compression_dictionary_tables.h"  // Compression_dictionary_tables
 #include "sql/dd/impl/system_views/enabled_roles.h"        // Enabled_roles
 #include "sql/dd/impl/system_views/events.h"               // Events
 #include "sql/dd/impl/system_views/files.h"                // Files
@@ -202,6 +204,8 @@ void System_tables::add_remaining_dd_tables() {
   register_table("backup_progress", system);
   register_table("columns_priv", system);
   register_table("component", system);
+  register_table("compression_dictionary", system);
+  register_table("compression_dictionary_cols", system);
   register_table("db", system);
   register_table("default_roles", system);
   register_table("engine_cost", system);
@@ -253,6 +257,9 @@ void System_views::init() {
   register_view<dd::system_views::Collation_charset_applicability>(is);
   register_view<dd::system_views::Columns>(is);
   register_view<dd::system_views::Column_statistics>(is);
+  register_view<dd::system_views::Compression_dictionary>(non_dd_based_is);
+  register_view<dd::system_views::Compression_dictionary_tables>(
+      non_dd_based_is);
   register_view<dd::system_views::Events>(is);
   register_view<dd::system_views::Files>(is);
   register_view<dd::system_views::Innodb_datafiles>(is);
