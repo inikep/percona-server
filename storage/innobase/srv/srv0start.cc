0293a856d97429e3bd63f025f676e1a67b92d657
Zsolt Parragi <zsolt.parragi@percona.com>
2018-11-27 14:51:11 +0100

    PS-5076: Redo log scrubbing ported to 8.0

--- storage/innobase/srv/srv0start.cc
+++ storage/innobase/srv/srv0start.cc
@@ -205,6 +205,7 @@ mysql_pfs_key_t srv_log_tracking_thread_key;
 mysql_pfs_key_t srv_worker_thread_key;
 mysql_pfs_key_t trx_recovery_rollback_thread_key;
 mysql_pfs_key_t srv_ts_alter_encrypt_thread_key;
+mysql_pfs_key_t log_scrub_thread_key;
 #endif /* UNIV_PFS_THREAD */
 
 #ifdef HAVE_PSI_STAGE_INTERFACE
@@ -498,6 +499,8 @@ static dberr_t create_log_files(char *logfilename, size_t dirnamelen, lsn_t lsn,
   we do the fsyncs now unconditionally and repeat the required
   flush just before the rename. */
   fil_flush_file_redo();
+
+  log_ensure_scrubbing_thread();
 
   return (DB_SUCCESS);
 }
@@ -1720,6 +1723,10 @@ void srv_shutdown_exit_threads() {
         srv_purge_wakeup();
       }
 
+      if (log_scrub_thread_active) {
+        os_event_set(log_scrub_event);
+      }
+
       /* Stop srv_redo_log_follow_thread thread */
       if (srv_thread_is_active(srv_threads.m_changed_page_tracker)) {
         os_event_reset(srv_redo_log_tracked_event);
@@ -2166,6 +2173,7 @@ dberr_t srv_start(bool create_new_db) {
 
   fsp_init();
   pars_init();
+
   log_online_init();
 
   recv_sys_create();
@@ -3446,8 +3454,6 @@ static void srv_shutdown_page_cleaners() {
 
   srv_shutdown_set_state(SRV_SHUTDOWN_FLUSH_PHASE);
 
-  std::atomic_thread_fence(std::memory_order_seq_cst);
-
   /* At this point only page_cleaner should be active. We wait
   here to let it complete the flushing of the buffer pools
   before proceeding further. */
@@ -3499,6 +3505,11 @@ static lsn_t srv_shutdown_log() {
   ut_a(srv_shutdown_state.load() == SRV_SHUTDOWN_FLUSH_PHASE);
   ut_a(!buf_flush_page_cleaner_is_active());
   ut_a(buf_pool_check_no_pending_io() == 0);
+
+  if (log_scrub_thread_active) {
+    ut_ad(!srv_read_only_mode);
+    os_event_set(log_scrub_event);
+  }
 
   if (srv_fast_shutdown == 2) {
     if (!srv_read_only_mode) {
@@ -3735,6 +3746,9 @@ void srv_shutdown() {
   log_online_shutdown();
   ddl_log_close();
   log_sys_close();
+  if (!srv_read_only_mode && srv_scrub_log) {
+    os_event_destroy(log_scrub_event);
+  }
   recv_sys_close();
   trx_sys_close();
   lock_sys_close();
@@ -3801,4 +3815,15 @@ void srv_fatal_error() {
   flush_error_log_messages();
 
   std::_Exit(3);
+}
+
+/* @} */
+
+void log_ensure_scrubbing_thread(void) {
+  log_scrub_thread_active = srv_scrub_log;
+  if (log_scrub_thread_active) {
+    log_scrub_event = os_event_create();
+    auto thread = os_thread_create(log_scrub_thread_key, log_scrub_thread);
+    thread.start();
+  }
 }
