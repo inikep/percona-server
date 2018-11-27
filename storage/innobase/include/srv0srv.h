5d19d824dc4d18cfbc0e21f86b83431cdaf63a4c
Zsolt Parragi <zsolt.parragi@percona.com>
2018-11-27 14:51:11 +0100

    PS-5076: Redo log scrubbing ported to 8.0

--- storage/innobase/include/srv0srv.h
+++ storage/innobase/include/srv0srv.h
@@ -157,6 +157,9 @@ struct srv_stats_t {
 
   /* Number of row log blocks decrypted */
   ulint_ctr_64_t n_rowlog_blocks_decrypted;
+
+  /** Number of log scrub operations */
+  ulint_ctr_64_t n_log_scrubs;
 };
 
 /** Structure which keeps shared future objects for InnoDB background
@@ -738,6 +741,9 @@ extern ulong srv_thread_sleep_delay;
 extern ulong srv_spin_wait_delay;
 extern ibool srv_priority_boost;
 
+/* TRUE if enable log scrubbing */
+extern bool srv_scrub_log;
+
 extern ulint srv_truncated_status_writes;
 
 #if defined UNIV_DEBUG || defined UNIV_IBUF_DEBUG
@@ -871,6 +877,7 @@ extern mysql_pfs_key_t srv_ts_alter_encrypt_thread_key;
 extern mysql_pfs_key_t parallel_read_thread_key;
 extern mysql_pfs_key_t parallel_read_ahead_thread_key;
 extern mysql_pfs_key_t srv_log_tracking_thread_key;
+extern mysql_pfs_key_t log_scrub_thread_key;
 #endif /* UNIV_PFS_THREAD */
 #endif /* !UNIV_HOTBACKUP */
 
@@ -1317,6 +1324,8 @@ struct export_var_t {
 
   fragmentation_stats_t innodb_fragmentation_stats; /*!< Fragmentation
                                            statistics */
+
+  int64_t innodb_scrub_log;
 };
 
 #ifndef UNIV_HOTBACKUP
