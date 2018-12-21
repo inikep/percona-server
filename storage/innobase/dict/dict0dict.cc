a5cd38a0cc04c04f80c4939801075c1d6ad88065
Satya Bodapati <satya.bodapati@percona.com>
2018-12-21 20:22:58 +0530

    [encryption] PS-5222 : PS crash "Assertion failure: dict0dict.cc:7451:space_id != SPACE_UNKNOWN"           during upgrade from PS5.7.23 to PS8.0.13-3 with --innodb_file_per_table=OFF
    Problem:
    --------
    
    On debug builds, when 5.7 is bootstrapped with innodb_file_per_table=OFF and then upgraded
    to 8.0, upgrade fails
    
    This is because, in 8.0, we detect whether mysql database is encrypted or not based on mysql.plugin
    table. We look for tablespace flags of "mysql.plugin" table.
    
    With innodb_file_per_table=OFF, mysql.plugin doesn't have tablespace and we assert space_id must be valid.
    
    Fix:
    ----
    Get space_id of "mysql.plugin" from table object (dict_table_t) and use that to determine if database is
    encrypted or not for upgrade

--- storage/innobase/dict/dict0dict.cc
+++ storage/innobase/dict/dict0dict.cc
@@ -6108,6 +6108,7 @@ dberr_t dict_set_compression(dict_table_t *table, const char *algorithm) {
   return err;
 }
 #endif /* !UNIV_HOTBACKUP */
+
 /** Get single compression dictionary id for the given
 (table id, column pos) pair.
 @param[in]	table_id	table id
@@ -6167,11 +6168,17 @@ dberr_t dict_get_dictionary_info_by_id(ulint dict_id, char **name,
   return err;
 }
 
-bool dict_detect_encryption(bool is_upgrade) {
+bool dict_detect_encryption(bool is_upgrade, space_id_t mysql_plugin_space) {
   bool encrypt_mysql = false;
   if (is_upgrade) {
+    if (mysql_plugin_space == SYSTEM_TABLE_SPACE) {
+      return (srv_sys_space.is_encrypted());
+    }
+
     space_id_t space_id = fil_space_get_id_by_name("mysql/plugin");
-    ut_ad(space_id != SPACE_UNKNOWN);
+    if (space_id == SPACE_UNKNOWN) {
+      return (false);
+    }
 
     fil_space_t *space = fil_space_get(space_id);
     ut_ad(space != nullptr);
