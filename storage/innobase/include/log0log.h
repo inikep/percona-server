2883cb9e07ad64fa49e6a5354760df5d24e650e9
Zsolt Parragi <zsolt.parragi@percona.com>
2019-05-31 14:47:32 +0200

    [encryption] PS-5565: Better versioning with keyring redo encryption
    Previously keyring redo encryption only selected a version when the
    redo log was created - changing the key version required recreating
    the redo log.
    
    With this change, individual pages all have separate versions. To
    accomplish this, we reuse the checksum field: during encryption, we
    assume that the checksum is correct, and replace it with the
    checksum of the encrypted data, plus the key version.
    During reads, we calculate the checksum again, and assume that the
    difference between the written data and the calculated value is the
    key version.
    An incorrect checksum will result in an incorrect key version,
    resulting in quite likely a decryption error.
    In the unlikely case that the checksum is wrong, but the resulting
    number refers to an existing key, the decrypted data will be
    garbage and log recovery will abort later.
    
    Since the redo key version now dynamic, it is exported as the
    innodb_encryption_redo_key_version status variable.
    
    To support this change, keyring handling of the redo log keys were
    extracted to the redo_log_key_mgr global variable. This variable is
    now the owner of all used keyring redo keys, and everything else
    uses pointers to keys managed by it.
    This way:
    * keyring key handling is extracted to a common place, without
      too much code duplicated in multiple files
    * key-copying races between threads changing the keys and others
    writing data to the redo log are prevented, as it is a simple
    pointer change now
    
    The commit also includes minor fixes for issues previously hidden
    by the limited usability of keyring redo encryption.
    
    (cherry picked from commit 92159da720a2e066202a8104666d0c541ed4fe54)
    
    PS-5839: Fix memory leak in keyring_key redo log encryption
    
    (cherry picked from commit 643bafe9bc882bf4c6495a00b311eeb953e6c0fc)

--- storage/innobase/include/log0log.h
+++ storage/innobase/include/log0log.h
@@ -54,6 +54,8 @@ this program; if not, write to the Free Software Foundation, Inc.,
 #include "log0types.h"
 #include "my_compiler.h"
 
+extern uint srv_redo_log_key_version;
+
 /** Prefix for name of log file, e.g. "ib_logfile" */
 constexpr const char *const ib_logfile_basename = "ib_logfile";
 
@@ -881,7 +883,7 @@ extern redo_log_encrypt_enum existing_redo_encryption_mode;
 
 const char *log_encrypt_name(redo_log_encrypt_enum val);
 
-void redo_rotate_default_key();
+void log_rotate_default_key();
 
 /** Write the encryption info into the log file header(the 3rd block).
 It just need to flush the file header block with current master key.
@@ -898,6 +900,9 @@ It will re-encrypt the redo log encryption metadata and write it to
 redo log file header.
 @return true if success. */
 bool log_rotate_encryption();
+
+/* Checks if there is a new redo key when using keyring encryption. */
+void log_check_new_key_version();
 
 /** Computes lsn up to which sync flush should be done or returns 0
 if there is no need to execute sync flush now.
