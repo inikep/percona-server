c75638e59f3ada41d32265d75d6fc8c2baca0d20
Zsolt Parragi <zsolt.parragi@percona.com>
2019-05-31 14:47:32 +0200

    [encryption] PS-5565: Better versioning with keyring redo encryption
    Previously keyring redo encryption only selected a version when the
    redo log was created - changing the key version required recreating
    the redo log.
    
    With this change, individual pages all have separate versions. To
    accomplish this, we reuse the checksum field: during encryption, we
    assume that the checksum is correct, and replace it with the
    checksum of the encrypted data, plus the key version.
    During reads, we calculate the checksum again, and assume that the
    difference between the written data and the calculated value is the
    key version.
    An incorrect checksum will result in an incorrect key version,
    resulting in quite likely a decryption error.
    In the unlikely case that the checksum is wrong, but the resulting
    number refers to an existing key, the decrypted data will be
    garbage and log recovery will abort later.
    
    Since the redo key version now dynamic, it is exported as the
    innodb_encryption_redo_key_version status variable.
    
    To support this change, keyring handling of the redo log keys were
    extracted to the redo_log_key_mgr global variable. This variable is
    now the owner of all used keyring redo keys, and everything else
    uses pointers to keys managed by it.
    This way:
    * keyring key handling is extracted to a common place, without
      too much code duplicated in multiple files
    * key-copying races between threads changing the keys and others
    writing data to the redo log are prevented, as it is a simple
    pointer change now
    
    The commit also includes minor fixes for issues previously hidden
    by the limited usability of keyring redo encryption.
    
    (cherry picked from commit 92159da720a2e066202a8104666d0c541ed4fe54)
    
    PS-5839: Fix memory leak in keyring_key redo log encryption
    
    (cherry picked from commit 643bafe9bc882bf4c6495a00b311eeb953e6c0fc)

--- storage/innobase/fsp/fsp0file.cc
+++ storage/innobase/fsp/fsp0file.cc
@@ -730,7 +730,7 @@ Datafile::ValidateOutput Datafile::validate_first_page(space_id_t space_id,
             static_cast<byte *>(ut_zalloc_nokey(Encryption::KEY_LEN));
       }
 #ifdef UNIV_ENCRYPT_DEBUG
-      fprintf(stderr, "Got from file %u:", m_space_id);
+      fprintf(stderr, "Got from file " SPACE_ID_PFS ":", m_space_id);
 #endif
 
       if (!fsp_header_get_encryption_key(m_flags, m_encryption_key,
