4792650102bafc41d465ec45c8ac44dc7d52f034
Zsolt Parragi <zsolt.parragi@percona.com>
2019-06-17 21:18:13 +0200

    [encryption] PS-5697: Improve redo log encryption error reporting
    * Fixed innodb fatal logger: using an error code with arguments resulted
      in double log write, once incorrectly (without parameters)
    * If encryption can't be turned on during startup, the server will abort
    * If encryption can't be turned on when it's requested in a connection,
      an error message will be returned to the client.
    
    This results in a different behavior than MySQL 8.0, which silently
    ignores these scenarios.
    
    (cherry picked from commit bb0840e05d3417a286890bd3c41ce839c044ffed)
    
    PS-5979: Redo log encryption initialization error during recovery
    
    Issue: If redo log encryption was first turned on during crash recovery,
    and the "ON" option was used, redo log encryption wasn't initialized
    properly.
    
    Fix: handling all options during encryption initialization

--- storage/innobase/handler/ha_innodb.cc
+++ storage/innobase/handler/ha_innodb.cc
@@ -4295,7 +4295,7 @@ static void innobase_post_recover() {
       srv_redo_log_encrypt = false;
     } else {
       /* Enable encryption for REDO log */
-      if (srv_enable_redo_encryption()) {
+      if (srv_enable_redo_encryption(nullptr)) {
         ut_ad(false);
         srv_redo_log_encrypt = false;
       }
@@ -4589,7 +4589,7 @@ bool innobase_fix_tablespaces_empty_uuid() {
        server run. These functions are also called later, when the master key is
        correctly set up, later in this function.
      */
-    if (srv_enable_redo_encryption()) {
+    if (srv_enable_redo_encryption(nullptr)) {
       srv_redo_log_encrypt = REDO_LOG_ENCRYPT_OFF;
     } else {
       log_rotate_default_key();
@@ -4629,7 +4629,7 @@ bool innobase_fix_tablespaces_empty_uuid() {
     return (true);
   }
 
-  if (srv_enable_redo_encryption()) {
+  if (srv_enable_redo_encryption(nullptr)) {
     srv_redo_log_encrypt = REDO_LOG_ENCRYPT_OFF;
   } else {
     log_rotate_default_key();
@@ -22020,7 +22020,6 @@ static int validate_innodb_redo_log_encrypt(THD *thd, SYS_VAR *var, void *save,
 
   if (!legit_value) return 1;
 
-  // TODO(laurynas) or = use; ?
   /* Set the default output to current value for all error cases. */
   *static_cast<ulong *>(save) = srv_redo_log_encrypt;
 
@@ -22952,9 +22951,9 @@ static void update_innodb_redo_log_encrypt(THD *thd, SYS_VAR *var,
       existing_redo_encryption_mode != target &&
       !(existing_redo_encryption_mode == REDO_LOG_ENCRYPT_MK &&
         target == REDO_LOG_ENCRYPT_ON)) {
-    ib::warn(ER_REDO_ENCRYPTION_CANT_BE_CHANGED,
-             log_encrypt_name(existing_redo_encryption_mode),
-             log_encrypt_name(static_cast<redo_log_encrypt_enum>(target)));
+    ib::error(ER_REDO_ENCRYPTION_CANT_BE_CHANGED,
+              log_encrypt_name(existing_redo_encryption_mode),
+              log_encrypt_name(static_cast<redo_log_encrypt_enum>(target)));
     ib_senderrf(thd, IB_LOG_LEVEL_WARN, ER_DA_REDO_ENCRYPTION_CANT_BE_CHANGED,
                 log_encrypt_name(existing_redo_encryption_mode),
                 log_encrypt_name(static_cast<redo_log_encrypt_enum>(target)));
@@ -22962,15 +22961,14 @@ static void update_innodb_redo_log_encrypt(THD *thd, SYS_VAR *var,
   }
 
   if (srv_read_only_mode) {
-    push_warning_printf(thd, Sql_condition::SL_WARNING, ER_WRONG_ARGUMENTS,
-                        " Redo log cannot be"
-                        " encrypted in innodb_read_only mode");
+    ib::error(ER_IB_MSG_1242);
+    ib_senderrf(thd, IB_LOG_LEVEL_WARN, ER_IB_MSG_1242);
     return;
   }
 
   if (target == REDO_LOG_ENCRYPT_MK || target == REDO_LOG_ENCRYPT_ON) {
     ut_ad(strlen(server_uuid) > 0);
-    if (srv_enable_redo_encryption_mk()) {
+    if (srv_enable_redo_encryption_mk(thd)) {
       return;
     }
     srv_redo_log_encrypt = target;
@@ -22979,7 +22977,7 @@ static void update_innodb_redo_log_encrypt(THD *thd, SYS_VAR *var,
 
   if (target == REDO_LOG_ENCRYPT_RK) {
     ut_ad(strlen(server_uuid) > 0);
-    if (srv_enable_redo_encryption_rk()) {
+    if (srv_enable_redo_encryption_rk(thd)) {
       return;
     }
 
