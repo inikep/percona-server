251572ca5fd5788d01d1ea53f6e3100168a9e07c
Zsolt Parragi <zsolt.parragi@percona.com>
2019-06-17 21:18:13 +0200

    [encryption] PS-5697: Improve redo log encryption error reporting
    * Fixed innodb fatal logger: using an error code with arguments resulted
      in double log write, once incorrectly (without parameters)
    * If encryption can't be turned on during startup, the server will abort
    * If encryption can't be turned on when it's requested in a connection,
      an error message will be returned to the client.
    
    This results in a different behavior than MySQL 8.0, which silently
    ignores these scenarios.
    
    (cherry picked from commit bb0840e05d3417a286890bd3c41ce839c044ffed)
    
    PS-5979: Redo log encryption initialization error during recovery
    
    Issue: If redo log encryption was first turned on during crash recovery,
    and the "ON" option was used, redo log encryption wasn't initialized
    properly.
    
    Fix: handling all options during encryption initialization

--- storage/innobase/handler/ha_innodb.cc
+++ storage/innobase/handler/ha_innodb.cc
@@ -4417,7 +4417,7 @@ static void innobase_post_recover() {
       srv_redo_log_encrypt = false;
     } else {
       /* Enable encryption for REDO log */
-      if (srv_enable_redo_encryption()) {
+      if (srv_enable_redo_encryption(nullptr)) {
         ut_ad(false);
         srv_redo_log_encrypt = false;
       }
@@ -4704,7 +4704,7 @@ bool innobase_fix_tablespaces_empty_uuid() {
        server run. These functions are also called later, when the master key is
        correctly set up, later in this function.
      */
-    if (srv_enable_redo_encryption()) {
+    if (srv_enable_redo_encryption(nullptr)) {
       srv_redo_log_encrypt = REDO_LOG_ENCRYPT_OFF;
     } else {
       log_rotate_default_key();
@@ -4744,7 +4744,7 @@ bool innobase_fix_tablespaces_empty_uuid() {
     return (true);
   }
 
-  if (srv_enable_redo_encryption()) {
+  if (srv_enable_redo_encryption(nullptr)) {
     srv_redo_log_encrypt = REDO_LOG_ENCRYPT_OFF;
   } else {
     log_rotate_default_key();
@@ -22265,7 +22265,6 @@ static int validate_innodb_redo_log_encrypt(THD *thd, SYS_VAR *var, void *save,
 
   if (!legit_value) return 1;
 
-  // TODO(laurynas) or = use; ?
   /* Set the default output to current value for all error cases. */
   *static_cast<ulong *>(save) = srv_redo_log_encrypt;
 
