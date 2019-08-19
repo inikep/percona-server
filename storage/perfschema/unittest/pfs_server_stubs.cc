dbf6ed4e8cec4979239cc6a918cfc869c5af040d
Zsolt Parragi <zsolt.parragi@percona.com>
2019-08-19 13:58:12 +0200

    [utility_user] PS-5216: Port utility user to 8.0
    PS-5952, PS-5956: Fix utility user bugs
    
    PS-5952: Utility user visible in performance_schema.threads
    PS-5956: Root session must not be allowed to kill Utility user session
    
    (cherry picked from commit c6ac4db9d10be35526ff755aa7963c863bafa643)
    (cherry picked from commit 14718d380573a30c4e328dc06a3c7eb1bd16ad05)

--- storage/perfschema/unittest/pfs_server_stubs.cc
+++ storage/perfschema/unittest/pfs_server_stubs.cc
@@ -77,3 +77,7 @@ int log_message(int, ...) {
   /* Do not pollute the unit test output with annoying messages. */
   return 0;
 }
+
+bool acl_is_utility_user(const char *, const char *, const char *) {
+  return false;
+}
