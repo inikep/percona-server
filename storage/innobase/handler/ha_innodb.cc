56888f0c28169502b492f9ea9449af6c15d9020f
Yura Sorokin <yura.sorokin@percona.com>
2019-08-22 16:11:17 +0300

    PS-5363 (Merge MySQL 8.0.17): split Server Log / client error codes
    After WL#11678
    "Logging services: Add unique error-ids to the messages logged using error_log_printf()"
    (commit mysql/mysql-server@682bc7d)
    Oracle reorganized error codes into the following ranges:
    *  1000.. 1999 - server-to-client messages =< 5.6
    *  2000.. 2999 - reserved
    *  3000.. 3499 - server-to-client messages 5.7
    *  3500.. 4999 - server-to-client messages 8.0
    *  5000.. 5999 - reserved for X Plugin
    * 10000..49999 - server log messages
    * 50000..51999 - reserved
    
    As 6000..9999 is still a free range suitable for server-to-client messages,
    it can be reused for Percona Server.
    
    This commit puts all Percona-specific server-to-client messages into
    7000..7999 range - they are now marked with 'ER_DA_' prefix (DA stands for
    Diagnostic Area).
    RocksDB server-to-client messages are now in 8000..8999 range.
    
    Percona Server 8.0 server log messages remain in 48000..48999 range.
    
    Changed some error codes passed to 'ib_senderrf()' and
    'ib::error()' / 'ib::warn()' - basically we pass 'ER_DA_*' to 'ib_senderrf()'
    and 'ER_' to 'ib::error()' / 'ib::warn()'.
    Some of the encryption-related 'ER_*' error messages were duplicated in the
    server-to-client section as 'ER_DA_*'.
    
    Re-recorded some MTR test cases because of the changes error code numbers.

--- storage/innobase/handler/ha_innodb.cc
+++ storage/innobase/handler/ha_innodb.cc
@@ -5544,8 +5544,9 @@ static int innobase_init_files(dict_init_mode_t dict_init_mode,
   bool ret = dict_detect_encryption_of_mysql_ibd(
       dict_init_mode, upgrade_mysql_plugin_space, do_encrypt);
   if (!ret) {
-    ib::error(ER_XB_MSG_4) << "Failed to determine if mysql.ibd is encrypted. "
-                              "Have you deleted it?";
+    ib::error(ER_XB_MSG_4, "mysql.ibd")
+        << "Failed to determine if mysql.ibd is encrypted. "
+           "Have you deleted it?";
     return innodb_init_abort();
   }
 
@@ -5612,9 +5613,6 @@ static int innobase_init_files(dict_init_mode_t dict_init_mode,
   } else {
     return innodb_init_abort();
   }
-
-  /* Create mutex to protect encryption master_key_id. */
-  mutex_create(LATCH_ID_MASTER_KEY_ID_MUTEX, &master_key_id_mutex);
 
   innobase_old_blocks_pct = static_cast<uint>(
       buf_LRU_old_ratio_update(innobase_old_blocks_pct, TRUE));
@@ -18285,7 +18283,8 @@ int ha_innobase::check(THD *thd,                /*!< in: user thread handle */
         is_ok = false;
 
         if (err == DB_IO_DECRYPT_FAIL) {
-          ib_senderrf(thd, IB_LOG_LEVEL_ERROR, ER_XB_MSG_4,
+          ib_senderrf(thd, IB_LOG_LEVEL_ERROR,
+                      ER_DA_ENCRYPTION_TABLE_CHECK_FAILED,
                       index->table->name.m_name);
         } else {
           push_warning_printf(thd, Sql_condition::SL_WARNING, ER_NOT_KEYFILE,
@@ -18356,7 +18355,8 @@ int ha_innobase::check(THD *thd,                /*!< in: user thread handle */
     }
     if (ret != DB_SUCCESS) {
       if (ret == DB_IO_DECRYPT_FAIL) {
-        ib_senderrf(thd, IB_LOG_LEVEL_ERROR, ER_XB_MSG_4,
+        ib_senderrf(thd, IB_LOG_LEVEL_ERROR,
+                    ER_DA_ENCRYPTION_TABLE_CHECK_FAILED,
                     index->table->name.m_name);
       } else {
         /* Assume some kind of corruption. */
@@ -22237,7 +22237,7 @@ static void update_innodb_redo_log_encrypt(THD *thd, SYS_VAR *var,
     ib::warn(ER_REDO_ENCRYPTION_CANT_BE_CHANGED,
              log_encrypt_name(existing_redo_encryption_mode),
              log_encrypt_name(static_cast<redo_log_encrypt_enum>(target)));
-    ib_senderrf(thd, IB_LOG_LEVEL_WARN, ER_REDO_ENCRYPTION_CANT_BE_CHANGED,
+    ib_senderrf(thd, IB_LOG_LEVEL_WARN, ER_DA_REDO_ENCRYPTION_CANT_BE_CHANGED,
                 log_encrypt_name(existing_redo_encryption_mode),
                 log_encrypt_name(static_cast<redo_log_encrypt_enum>(target)));
     return;
