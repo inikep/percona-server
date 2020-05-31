3e003de3615298f1aeeb88130530ea41034641d0
Satya Bodapati <satya.bodapati@percona.com>
2020-05-31 14:45:13 +0530

    [encryption] PS-6789: Re-enabled PS-3822 (InnoDB system tablespace encryption)
    PS-6789 (Merge MySQL 8.0.20): Re-enabled PS-3822 (InnoDB system tablespace encryption)
    
    https://jira.percona.com/browse/PS-6789
    
    1. Re-enable system tablespace encryption again after 8.0.20 upstream merge
       that has new parallel doublewrite implementation
       (https://jira.percona.com/browse/PS-3822).
    2. Removed 'innodb.percona_sys_tablespace_encrypt_dblwr' MTR test case as
       there is no doublewrite buffer in system tablespace anymore.

--- storage/innobase/include/srv0srv.h
+++ storage/innobase/include/srv0srv.h
@@ -493,6 +493,9 @@ page size | FSP_EXTENT_SIZE  | Initial Size | Pages
 /** Enable or disable encryption of temporary tablespace.*/
 extern bool srv_tmp_tablespace_encrypt;
 
+/** Enable this option to encrypt system tablespace at bootstrap. */
+extern bool srv_sys_tablespace_encrypt;
+
 /** Whether the redo log tracking is currently enabled. Note that it is
 possible for the log tracker thread to be running and the tracking to be
 disabled */
