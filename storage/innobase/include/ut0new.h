0a033706e9b6d22b060c68a072b5f08330233ccc
Oleksandr Kachan <oleksandr.kachan@percona.com>
2021-08-10 23:58:58 +0300

    PS-7811 merge: Merge MySQL 8.0.26 (fix gcc-5 build)
    https://jira.percona.com/browse/PS-7811
    
    - The upstream change https://github.com/mysql/mysql-server/commit/2e273ed403827fa3d86970b40280d4fe9d154c43
      adds usage of std::unordered_map with enum class used as the map key.
      This leads to gcc-5 build failure as it lacks std::hash specialization
      for enums.
      Add custom hash function to be used for class_map_t unordered_map.
    - The ut_allocator::m_key made a const class memebr as a part of
      https://github.com/mysql/mysql-server/commit/b04ce7e067b495ab212162a4e23db5d6fc0b5da7 change.
      This change with gcc-5 makes it impossible to define assignment and
      move assignment operators which are required for std::move to work
      properly when an element is pushed into a container which uses
      ut_allocator as a custom allocator.
      Reverted ut_allocator::m_key to be a non-const class member.
    - The ut_allocator::construct() method was removed in
      https://github.com/mysql/mysql-server/commit/9620c59d44a71e3fac22d1698acea95cad170f4c.
      For gcc-5 it's still required for std::list which uses ut_allocator
      as a custom allocator to properly allocate internal node.
      Reimplement ut_allocator::construct() method to fix gcc-5 build.

--- storage/innobase/include/ut0new.h
+++ storage/innobase/include/ut0new.h
@@ -123,6 +123,7 @@ this program; if not, write to the Free Software Foundation, Inc.,
 #include <memory>
 #include <type_traits> /* std::is_trivially_default_constructible */
 #include <unordered_set>
+#include <utility>
 
 #include "my_basename.h"
 #include "mysql/components/services/bits/psi_bits.h"
