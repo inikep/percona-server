3c673fb35390fcc34b0e50c195dacd570978b676
Artyom Belov <avb@tempesta-tech.com>
2022-03-10 13:31:05 +0300

    PS-5008: Memory leak in sync_latch_meta_init() after mysqld shutdown detected by ASan
    Free all the resources acquired by InnoDB (mutexes, events, memory) in
    case of a failed start (errors in
    storage/innobase/srv/srv0start.cc::innobase_start_or_create_for_mysql())
    
    Removed silencing leaks in tests
    
    Added missing `innodb_init_abort()` in `innodb_init()`. It caused leaks
    
    Added missing `recv_recovery_from_checkpoint_finish()` in `srv_start()`. It caused leaks

--- storage/innobase/dict/dict0dict.cc
+++ storage/innobase/dict/dict0dict.cc
@@ -3992,6 +3992,8 @@ void dict_persist_init() {
 
 /** Clear the structure */
 void dict_persist_close() {
+  if (!dict_persist) return;
+
   ut::delete_(dict_persist->persisters);
 
 #ifndef UNIV_HOTBACKUP
--- storage/innobase/handler/ha_innodb.cc
+++ storage/innobase/handler/ha_innodb.cc
@@ -1815,9 +1815,9 @@ static int innodb_shutdown(handlerton *, ha_panic_function) {
     mysql_mutex_destroy(&resume_encryption_cond_m);
     mysql_cond_destroy(&resume_encryption_cond);
     os_event_destroy(recovery_lock_taken);
-
-    os_event_global_destroy();
   }
+
+  os_event_global_destroy();
 
   innobase::component_services::deinitialize_service_handles();
 
@@ -5909,8 +5909,8 @@ static int innodb_init(void *p) {
 
 #endif /* HAVE_PSI_INTERFACE */
 
-  if (int error = innodb_init_params()) {
-    return error;
+  if (innodb_init_params()) {
+    return innodb_init_abort();
   }
 
   /* After this point, error handling has to use innodb_init_abort(). */
