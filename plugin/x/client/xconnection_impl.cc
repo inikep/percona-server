94fe3d4380dfb63894a5f639a499c1afbc8fddd8
Yura Sorokin <yura.sorokin@percona.com>
2022-04-13 11:59:27 +0000

    PS-8166: Add OpenSSL 3.0.x support
    https://jira.percona.com/browse/PS-8166
    
    Applied OpenSSL 3.0.x ('ssl3') patch from 'mysql-server-8.0' Ubuntu 22.04
    Jammy Jellyfish source package.

--- plugin/x/client/xconnection_impl.cc
+++ plugin/x/client/xconnection_impl.cc
@@ -33,6 +33,10 @@
 #ifdef HAVE_NETINET_IN_H
 #include <netinet/in.h>
 #endif
+#if OPENSSL_VERSION_NUMBER >= 0x30000000L
+#include <openssl/evp.h>
+#include <openssl/provider.h>
+#endif
 #include <cassert>
 #include <limits>
 #include <sstream>
@@ -470,12 +474,21 @@ int set_fips_mode(const uint fips_mode, char err_string[OPENSSL_ERROR_LENGTH]) {
   if (fips_mode > 2) {
     goto EXIT;
   }
+#if OPENSSL_VERSION_NUMBER >= 0x30000000L
+  fips_mode_old = EVP_default_properties_is_fips_enabled(NULL) &&
+                  OSSL_PROVIDER_available(NULL, "fips");
+#else
   fips_mode_old = FIPS_mode();
+#endif
   if (fips_mode_old == fips_mode) {
     rc = 1;
     goto EXIT;
   }
+#if OPENSSL_VERSION_NUMBER >= 0x30000000L
+  if (!(rc = EVP_default_properties_enable_fips(NULL, fips_mode))) {
+#else
   if (!(rc = FIPS_mode_set(fips_mode))) {
+#endif
     err_library = ERR_get_error();
     ERR_error_string_n(err_library, err_string, OPENSSL_ERROR_LENGTH - 1);
     err_string[OPENSSL_ERROR_LENGTH - 1] = '\0';
