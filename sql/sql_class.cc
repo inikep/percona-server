656b0e88a6457971001d076363500af837d25597
Przemyslaw Skibinski <przemyslaw.skibinski@percona.com>
2022-06-09 16:02:21 +0200

    [vio] PS-269: Initial Percona Server 8.0.12 tree

--- sql/sql_class.cc
+++ sql/sql_class.cc
@@ -1911,7 +1911,7 @@ void THD::shutdown_active_vio() {
   DBUG_TRACE;
   mysql_mutex_assert_owner(&LOCK_thd_data);
   if (active_vio) {
-    vio_shutdown(active_vio);
+    vio_shutdown(active_vio, SHUT_RDWR);
     active_vio = nullptr;
     m_SSL = nullptr;
   }
@@ -1921,7 +1921,7 @@ void THD::shutdown_clone_vio() {
   DBUG_TRACE;
   mysql_mutex_assert_owner(&LOCK_thd_data);
   if (clone_vio != nullptr) {
-    vio_shutdown(clone_vio);
+    vio_shutdown(clone_vio, SHUT_RDWR);
     clone_vio = nullptr;
   }
 }
