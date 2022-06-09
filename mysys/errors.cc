538db71739f9691ed9d300bccecdf58cbdff3356
Przemyslaw Skibinski <przemyslaw.skibinski@percona.com>
2022-06-09 15:30:09 +0200

    [mysys] PS-269: Initial Percona Server 8.0.12 tree

--- mysys/errors.cc
+++ mysys/errors.cc
@@ -139,7 +139,9 @@ const char *globerrs[GLOBERRS] = {
     "Reset character out of range: %s.",
     "Unknown LDML tag: '%.*s'.",
     "Failed to reset before a secondary ignorable character %s.",
-    "Stopped processing the '%s' directive in file %s at line %d."};
+    "Stopped processing the '%s' directive in file %s at line %d.",
+    "Can't create socket '%s' (Errcode: %d)",
+    "File name '%s' is too long (max: %d)"};
 
 /*
  We cannot call my_error/my_printf_error here in this function.
