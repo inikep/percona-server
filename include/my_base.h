d4162f26a14defac087764b76c1dbce29f7a226a
Przemyslaw Skibinski <przemyslaw.skibinski@percona.com>
2022-06-09 15:30:09 +0200

    Percona Server 8.0 (merge 5.7 as of 2018-08-08) - mysys

--- include/my_base.h
+++ include/my_base.h
@@ -494,11 +494,14 @@ enum ha_base_keytype {
 #define HA_NULL_ARE_EQUAL 2048
 /** Key was automatically created to support Foreign Key constraint. */
 #define HA_GENERATED_KEY 8192
+/** TokuDB CLUSTERING key */
+#define HA_CLUSTERING (1 << 31)
 
 /* The combination of the above can be used for key type comparison. */
 #define HA_KEYFLAG_MASK                                                       \
   (HA_NOSAME | HA_PACK_KEY | HA_AUTO_KEY | HA_BINARY_PACK_KEY | HA_FULLTEXT | \
-   HA_UNIQUE_CHECK | HA_SPATIAL | HA_NULL_ARE_EQUAL | HA_GENERATED_KEY)
+   HA_UNIQUE_CHECK | HA_SPATIAL | HA_NULL_ARE_EQUAL | HA_GENERATED_KEY |      \
+   HA_CLUSTERING)
 
 /**
   Key was renamed (or is result of renaming a key).
@@ -921,7 +924,10 @@ is the global server default. */
 #define HA_ERR_NO_WAIT_LOCK 203     /* Don't wait for record lock */
 #define HA_ERR_DISK_FULL_NOWAIT 204 /* No more room in disk */
 #define HA_ERR_NO_SESSION_TEMP 205  /* No session temporary space available */
-#define HA_ERR_LAST 205             /* Copy of last error nr */
+#define HA_ERR_DEST_SCHEMA_NOT_EXIST                         \
+  206                   /* Destination schema does not exist \
+                         */
+#define HA_ERR_LAST 206 /* Copy of last error nr */
 
 /* Number of different errors */
 #define HA_ERR_ERRORS (HA_ERR_LAST - HA_ERR_FIRST + 1)
