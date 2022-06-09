8b23d9f6716a79eb1f84572c5870220b0c674875
Przemyslaw Skibinski <przemyslaw.skibinski@percona.com>
2022-06-09 21:25:26 +0200

    Percona Server 8.0 (merge 5.7 as of 2018-08-08) - include

--- include/mysql.h.pp
+++ include/mysql.h.pp
@@ -85,6 +85,7 @@ enum enum_server_command {
   COM_CLONE,
   COM_END
 };
+static const int PURGE_BITMAPS_TO_LSN = 1;
 enum SERVER_STATUS_flags_enum {
   SERVER_STATUS_IN_TRANS = 1,
   SERVER_STATUS_AUTOCOMMIT = 2,
@@ -243,7 +244,7 @@ bool my_thread_init(void);
 void my_thread_end(void);
 unsigned long net_field_length(unsigned char **packet);
 unsigned long net_field_length_checked(unsigned char **packet,
-                                       unsigned long max_length);
+                                               unsigned long max_length);
 unsigned long long net_field_length_ll(unsigned char **packet);
 unsigned char *net_store_length(unsigned char *pkg, unsigned long long length);
 unsigned int net_length_size(unsigned long long num);
@@ -308,6 +309,7 @@ typedef struct MYSQL_TIME {
 void init_client_errs(void);
 void finish_client_errs(void);
 extern const char *client_errors[];
+extern const char **mysql_client_errors;
 static inline const char *ER_CLIENT(int client_errno) {
   if (client_errno >= 2000 && client_errno <= 2064)
     return client_errors[client_errno - 2000];
