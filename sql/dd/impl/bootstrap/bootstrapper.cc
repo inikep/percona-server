09ef585582d221ac3c1223fe6cc08daa34f818fd
Przemyslaw Skibinski <przemyslaw.skibinski@percona.com>
2022-06-29 13:12:03 +0200

    check_and_create_compression_dict_tables

--- sql/dd/impl/bootstrap/bootstrapper.cc
+++ sql/dd/impl/bootstrap/bootstrapper.cc
@@ -910,6 +910,113 @@ bool initialize(THD *thd) {
   return false;
 }
 
+/** On startup from mysql datadir to Percona Server, compression dictionary
+tables and I_S views on them will be missing. We check if they are missing
+and create the tables mysql.compression_dictionary,
+mysql.compression_dictionary_cols
+@param[in,out]  thd  Session context
+@return false on success, true on failure */
+static bool check_and_create_compression_dict_tables(THD *thd) {
+  const dd::Table *comp_table_def = nullptr;
+  if (thd->dd_client()->acquire("mysql", "compression_dictionary",
+                                &comp_table_def)) {
+    return true;
+  }
+
+  const dd::Table *comp_cols_table_def = nullptr;
+  if (thd->dd_client()->acquire("mysql", "compression_dictionary_cols",
+                                &comp_cols_table_def)) {
+    return true;
+  }
+
+  const dd::Table *view_table_def = nullptr;
+  if (thd->dd_client()->acquire("mysql", "view_table_usage", &view_table_def)) {
+    return true;
+  }
+
+  /*
+    If compression dictionary tables were created by upgrade from 5.7 to
+    PS-8.0.13-3, the table_ids would be wrong. Use the first available hardcoded
+    table_id
+  */
+  bool fix_table_ids = false;
+
+  if (comp_table_def != nullptr && comp_cols_table_def != nullptr) {
+    uint64 comp_table_id = comp_table_def->se_private_id();
+    uint64 comp_cols_table_id = comp_cols_table_def->se_private_id();
+    uint64 view_table_id = view_table_def->se_private_id();
+
+    if (comp_table_id == view_table_id + 1 &&
+        (comp_cols_table_id == view_table_id + 2)) {
+      return false;
+    } else {
+      fix_table_ids = true;
+    }
+  }
+
+  /*
+    If we are in read-only mode, we skip re-populating. Here, 'opt_readonly'
+    is the value of the '--read-only' option.
+  */
+  if (opt_readonly) {
+    LogErr(WARNING_LEVEL, ER_COMPRESSION_DICTIONARY_NO_CREATE, "", "");
+    return false;
+  }
+
+  /*
+    We must also check if the DDSE is started in a way that makes the DD
+    read only. For now, we only support InnoDB as SE for the DD. The call
+    to retrieve the handlerton for the DDSE should be replaced by a more
+    generic mechanism.
+  */
+  handlerton *ddse = ha_resolve_by_legacy_type(thd, DB_TYPE_INNODB);
+
+  if (ddse->is_dict_readonly && ddse->is_dict_readonly()) {
+    LogErr(WARNING_LEVEL, ER_COMPRESSION_DICTIONARY_NO_CREATE, "InnoDB", " ");
+    return false;
+  }
+
+  DBUG_EXECUTE_IF("skip_compression_dict_fix", fix_table_ids = false;);
+
+  if (fix_table_ids) {
+    dd::Table *comp_dict = nullptr;
+    if (thd->dd_client()->acquire_for_modification(
+            "mysql", "compression_dictionary", &comp_dict)) {
+    }
+
+    dd::Table *comp_cols_dict = nullptr;
+    if (thd->dd_client()->acquire_for_modification(
+            "mysql", "compression_dictionary_cols", &comp_cols_dict)) {
+    }
+
+    comp_dict->set_se_private_id(view_table_def->se_private_id() + 1);
+    comp_cols_dict->set_se_private_id(comp_dict->se_private_id() + 1);
+
+    if (thd->dd_client()->update<dd::Table>(comp_dict)) {
+      return true;
+    }
+
+    if (thd->dd_client()->update<dd::Table>(comp_cols_dict)) {
+      return true;
+    }
+
+    return (false);
+  }
+
+  // Create the compression dictionary tables
+  if (compression_dict::bootstrap(thd)) return true;
+
+  if (dd::info_schema::create_non_dd_views(thd, false)) {
+    return true;
+  }
+
+  /*
+    We must commit the transaction before executing a new query, which
+    expects the transaction to be empty.
+  */
+  return (dd::end_transaction(thd, false));
+}
+
 // Normal server restart.
 bool restart(THD *thd) {
   bootstrap::DD_bootstrap_ctx::instance().set_stage(bootstrap::Stage::STARTED);
@@ -937,6 +1044,7 @@ bool restart(THD *thd) {
       DDSE_dict_recover(thd, DICT_RECOVERY_RESTART_SERVER,
                         d->get_actual_dd_version(thd)) ||
       upgrade::do_server_upgrade_checks(thd) || upgrade::upgrade_tables(thd) ||
+      check_and_create_compression_dict_tables(thd) ||
       repopulate_charsets_and_collations(thd) || verify_contents(thd) ||
       update_versions(thd, false)) {
     return true;
