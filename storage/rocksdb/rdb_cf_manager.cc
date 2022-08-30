facebook/mysql-5.6

Change column family handle to shared_ptr and remove metadata for dropping column family and index
Summary:
This is the quip document about the design: https://fb.quip.com/IymkAe4VdDFv

This is not the final diff, it is for CR discussion
This diff contains 2 major components:
1. Used std::shared_ptr for ColumnFamilyHandle object. The object is a member of Rdb_cf_manager and Rdb_key_def classes. I made the member object of both classes shared_ptr. For all other places that use this object, I checked if shared_ptr is needed or a raw pointer is sufficient.
2. Added locking between Dropping a CF and creating a table with an index using the same CF. On dropping a CF we already have Rdb_cf_manager lock protecting the entire operation. The creation path has 2 steps: 1. get or create (if non-existent) CFs and 2. record the CFs to meta data dictionary. My design is that before step 2 accquire Rdb_cf_manager lock, then check if all the CFs referenced by the new table are in the map of the cf_manager. If any of them (new or existing) is not in the map, it means it has be dropped. Then we fail table creation. Client can retry with the same CREATE TABLE statement.

I did manual verification in debugger against the change.

I added 8 mtr tests to validate the change under various race conditions dropping cf can bring to the server.

Differential Revision: D12973157

fbshipit-source-id: a061dabab10

storage/rocksdb/rdb_cf_manager.cc
@@ -18,16 +18,19 @@
 #pragma implementation  // gcc: Class implementation
 #endif
 
+#include "./debug_sync.h"
+
+#include "./rdb_threads.h"
 /* This C++ files header file */
 #include "./rdb_cf_manager.h"
 
 /* MyRocks header files */
+#include <string>
+#include <vector>
 #include "./ha_rocksdb.h"
 #include "./ha_rocksdb_proto.h"
 #include "./rdb_datadic.h"
 #include "./rdb_psi.h"
-
-#include <string>
 
 namespace myrocks {
 
@@ -49,17 +52,18 @@ void Rdb_cf_manager::init(
 
   m_cf_options = std::move(cf_options);
 
-  for (auto cfh : *handles) {
-    DBUG_ASSERT(cfh != nullptr);
-    m_cf_name_map[cfh->GetName()] = cfh;
-    m_cf_id_map[cfh->GetID()] = cfh;
+  for (auto cfh_ptr : *handles) {
+    DBUG_ASSERT(cfh_ptr != nullptr);
+
+    std::shared_ptr<rocksdb::ColumnFamilyHandle> cfh(cfh_ptr);
+    m_cf_name_map[cfh_ptr->GetName()] = cfh;
+    m_cf_id_map[cfh_ptr->GetID()] = cfh;
   }
 }
 
 void Rdb_cf_manager::cleanup() {
-  for (const auto &it : m_cf_name_map) {
-    delete it.second;
-  }
+  m_cf_name_map.clear();
+  m_cf_id_map.clear();
   mysql_mutex_destroy(&m_mutex);
   m_cf_options = nullptr;
 }
@@ -71,16 +75,16 @@ void Rdb_cf_manager::cleanup() {
   @detail
     See Rdb_cf_manager::get_cf
 */
-rocksdb::ColumnFamilyHandle *Rdb_cf_manager::get_or_create_cf(
+std::shared_ptr<rocksdb::ColumnFamilyHandle> Rdb_cf_manager::get_or_create_cf(
     rocksdb::DB *const rdb, const std::string &cf_name_arg) {
   DBUG_ASSERT(rdb != nullptr);
 
-  rocksdb::ColumnFamilyHandle *cf_handle = nullptr;
+  std::shared_ptr<rocksdb::ColumnFamilyHandle> cf_handle;
 
   if (cf_name_arg == PER_INDEX_CF_NAME) {
     // per-index column families is no longer supported.
     my_error(ER_PER_INDEX_CF_DEPRECATED, MYF(0));
-    return nullptr;
+    return cf_handle;
   }
 
   const std::string &cf_name =
@@ -107,14 +111,15 @@ rocksdb::ColumnFamilyHandle *Rdb_cf_manager::get_or_create_cf(
     sql_print_information("    target_file_size_base=%" PRIu64,
                           opts.target_file_size_base);
 
+    rocksdb::ColumnFamilyHandle *cf_handle_ptr = nullptr;
     const rocksdb::Status s =
-        rdb->CreateColumnFamily(opts, cf_name, &cf_handle);
+        rdb->CreateColumnFamily(opts, cf_name, &cf_handle_ptr);
 
     if (s.ok()) {
-      m_cf_name_map[cf_handle->GetName()] = cf_handle;
-      m_cf_id_map[cf_handle->GetID()] = cf_handle;
-    } else {
-      cf_handle = nullptr;
+      DBUG_ASSERT(cf_handle_ptr != nullptr);
+      cf_handle.reset(cf_handle_ptr);
+      m_cf_name_map[cf_handle_ptr->GetName()] = cf_handle;
+      m_cf_id_map[cf_handle_ptr->GetID()] = cf_handle;
     }
   }
 
@@ -126,14 +131,14 @@ rocksdb::ColumnFamilyHandle *Rdb_cf_manager::get_or_create_cf(
 /*
   Find column family by its cf_name.
 */
-rocksdb::ColumnFamilyHandle *
-Rdb_cf_manager::get_cf(const std::string &cf_name_arg) const {
+std::shared_ptr<rocksdb::ColumnFamilyHandle> Rdb_cf_manager::get_cf(
+    const std::string &cf_name_arg) const {
   return get_cf(cf_name_arg, false /*lock_held_by_caller*/);
 }
 
-rocksdb::ColumnFamilyHandle *Rdb_cf_manager::get_cf(
+std::shared_ptr<rocksdb::ColumnFamilyHandle> Rdb_cf_manager::get_cf(
     const std::string &cf_name_arg, const bool lock_held_by_caller) const {
-  rocksdb::ColumnFamilyHandle *cf_handle;
+  std::shared_ptr<rocksdb::ColumnFamilyHandle> cf_handle;
 
   if (!lock_held_by_caller) {
     RDB_MUTEX_LOCK_CHECK(m_mutex);
@@ -141,7 +146,10 @@ rocksdb::ColumnFamilyHandle *Rdb_cf_manager::get_cf(
   std::string cf_name = cf_name_arg.empty() ? DEFAULT_CF_NAME : cf_name_arg;
 
   const auto it = m_cf_name_map.find(cf_name);
-  cf_handle = (it != m_cf_name_map.end()) ? it->second : nullptr;
+
+  if (it != m_cf_name_map.end()) {
+    cf_handle = it->second;
+  }
 
   if (!cf_handle) {
     // NO_LINT_DEBUG
@@ -155,8 +163,9 @@ rocksdb::ColumnFamilyHandle *Rdb_cf_manager::get_cf(
   return cf_handle;
 }
 
-rocksdb::ColumnFamilyHandle *Rdb_cf_manager::get_cf(const uint32_t id) const {
-  rocksdb::ColumnFamilyHandle *cf_handle = nullptr;
+std::shared_ptr<rocksdb::ColumnFamilyHandle> Rdb_cf_manager::get_cf(
+    const uint32_t id) const {
+  std::shared_ptr<rocksdb::ColumnFamilyHandle> cf_handle;
 
   RDB_MUTEX_LOCK_CHECK(m_mutex);
   const auto it = m_cf_id_map.find(id);
@@ -178,9 +187,9 @@ std::vector<std::string> Rdb_cf_manager::get_cf_names(void) const {
   return names;
 }
 
-std::vector<rocksdb::ColumnFamilyHandle *> Rdb_cf_manager::get_all_cf(
-    void) const {
-  std::vector<rocksdb::ColumnFamilyHandle *> list;
+std::vector<std::shared_ptr<rocksdb::ColumnFamilyHandle>>
+Rdb_cf_manager::get_all_cf(void) const {
+  std::vector<std::shared_ptr<rocksdb::ColumnFamilyHandle>> list;
 
   RDB_MUTEX_LOCK_CHECK(m_mutex);
 
@@ -194,70 +203,68 @@ std::vector<rocksdb::ColumnFamilyHandle *> Rdb_cf_manager::get_all_cf(
   return list;
 }
 
-namespace {
-
-struct Rdb_cf_scanner : public Rdb_tables_scanner {
-  uint32_t m_cf_id;
-  int m_is_cf_used;
-
-  explicit Rdb_cf_scanner(uint32_t cf_id)
-      : m_cf_id(cf_id), m_is_cf_used(false) {}
-
-  int add_table(Rdb_tbl_def *tdef) override {
-    DBUG_ASSERT(tdef != nullptr);
-
-    for (uint i = 0; i < tdef->m_key_count; i++) {
-      const Rdb_key_def &kd = *tdef->m_key_descr_arr[i];
-
-      if (kd.get_cf()->GetID() == m_cf_id) {
-        m_is_cf_used = true;
-        return HA_EXIT_SUCCESS;
-      }
-    }
-    return HA_EXIT_SUCCESS;
-  }
-};
-
-}  // namespace
-
-int Rdb_cf_manager::drop_cf(const std::string &cf_name) {
-  auto ddl_manager = rdb_get_ddl_manager();
-  uint32_t cf_id = 0;
-
-  if (cf_name == DEFAULT_SYSTEM_CF_NAME) {
-    return HA_EXIT_FAILURE;
-  }
-
+int Rdb_cf_manager::remove_dropped_cf(Rdb_dict_manager *const dict_manager,
+                                      rocksdb::TransactionDB *const rdb,
+                                      const uint32 &cf_id) {
+  dict_manager->assert_lock_held();
   RDB_MUTEX_LOCK_CHECK(m_mutex);
-  auto cf_handle = get_cf(cf_name, true /* lock_held_by_caller */);
-  if (cf_handle == nullptr) {
-    RDB_MUTEX_UNLOCK_CHECK(m_mutex);
-    return HA_EXIT_SUCCESS;
-  }
+  const std::unique_ptr<rocksdb::WriteBatch> wb = dict_manager->begin();
+  rocksdb::WriteBatch *const batch = wb.get();
 
-  cf_id = cf_handle->GetID();
-  Rdb_cf_scanner scanner(cf_id);
-
-  auto ret = ddl_manager->scan_for_tables(&scanner);
-  if (ret) {
+  const auto it = m_cf_id_map.find(cf_id);
+  if (it == m_cf_id_map.end()) {
+    dict_manager->delete_dropped_cf_and_flags(batch, cf_id);
+    dict_manager->commit(batch);
     RDB_MUTEX_UNLOCK_CHECK(m_mutex);
-    return ret;
-  }
 
-  if (scanner.m_is_cf_used) {
-    // column family is used by existing key
-    RDB_MUTEX_UNLOCK_CHECK(m_mutex);
+    // NO_LINT_DEBUG
+    sql_print_warning(
+        "RocksDB: Column family with id %u is marked as dropped, "
+        "but doesn't exist in cf manager",
+        cf_id);
+
     return HA_EXIT_FAILURE;
   }
 
-  auto rdb = rdb_get_rocksdb_db();
-  auto status = rdb->DropColumnFamily(cf_handle);
-  if (!status.ok()) {
+  auto cf_handle = it->second.get();
+  const std::string cf_name = cf_handle->GetName();
+
+  if (!dict_manager->get_dropped_cf(cf_id)) {
     RDB_MUTEX_UNLOCK_CHECK(m_mutex);
+    // NO_LINT_DEBUG
+    sql_print_warning(
+        "RocksDB: Column family %s with id %u is not in "
+        "the list of cf ids to be dropped",
+        cf_name.c_str(), cf_id);
+    return HA_EXIT_FAILURE;
+  }
+
+  auto status = rdb->DropColumnFamily(cf_handle);
+
+  if (!status.ok()) {
+    dict_manager->delete_dropped_cf(batch, cf_id);
+    dict_manager->commit(batch);
+    RDB_MUTEX_UNLOCK_CHECK(m_mutex);
+
+    // NO_LINT_DEBUG
+    sql_print_error(
+        "RocksDB: Dropping column family %s with id %u on RocksDB failed",
+        cf_name.c_str(), cf_id);
+
     return ha_rocksdb::rdb_error_to_mysql(status);
   }
 
-  delete cf_handle;
+  DBUG_EXECUTE_IF("rocksdb_remove_dropped_cf", {
+    THD *thd = new THD();
+    thd->thread_stack = reinterpret_cast<char *>(&(thd));
+    thd->store_globals();
+    const char act[] =
+        "now signal ready_to_restart_during_drop_cf wait_for "
+        "restart_done_during_drop_cf";
+    DBUG_ASSERT(!debug_sync_set_action(thd, STRING_WITH_LEN(act)));
+    thd->restore_globals();
+    delete thd;
+  });
 
   auto id_iter = m_cf_id_map.find(cf_id);
   DBUG_ASSERT(id_iter != m_cf_id_map.end());
@@ -267,8 +274,144 @@ int Rdb_cf_manager::drop_cf(const std::string &cf_name) {
   DBUG_ASSERT(name_iter != m_cf_name_map.end());
   m_cf_name_map.erase(name_iter);
 
+  dict_manager->delete_dropped_cf_and_flags(batch, cf_id);
+
+  dict_manager->commit(batch);
   RDB_MUTEX_UNLOCK_CHECK(m_mutex);
+
+  // NO_LINT_DEBUG
+  sql_print_information(
+      "RocksDB: Column family %s with id %u has been dropped successfully",
+      cf_name.c_str(), cf_id);
 
   return HA_EXIT_SUCCESS;
 }
+
+namespace {
+struct Rdb_cf_scanner : public Rdb_tables_scanner {
+  uint32_t m_cf_id;
+
+  explicit Rdb_cf_scanner(uint32_t cf_id) : m_cf_id(cf_id) {}
+
+  int add_table(Rdb_tbl_def *tdef) override {
+    DBUG_ASSERT(tdef != nullptr);
+
+    for (uint i = 0; i < tdef->m_key_count; i++) {
+      const Rdb_key_def &kd = *tdef->m_key_descr_arr[i];
+
+      if (kd.get_cf()->GetID() == m_cf_id) {
+        return HA_EXIT_FAILURE;
+      }
+    }
+    return HA_EXIT_SUCCESS;
+  }
+};
+
+}  // namespace
+
+int Rdb_cf_manager::drop_cf(Rdb_ddl_manager *const ddl_manager,
+                            Rdb_dict_manager *const dict_manager,
+                            const std::string &cf_name) {
+  dict_manager->assert_lock_held();
+  uint32_t cf_id = 0;
+
+  if (cf_name == DEFAULT_SYSTEM_CF_NAME || cf_name == DEFAULT_CF_NAME ||
+      cf_name.empty()) {
+    return HA_EXIT_FAILURE;
+  }
+
+  // We should have already acquired dict manager lock. The order
+  // of lock acquisition here is:
+  //  dict_manager -> cf_manager -> ddl_manager
+
+  RDB_MUTEX_LOCK_CHECK(m_mutex);
+  auto cf_handle = get_cf(cf_name, true /* lock_held_by_caller */).get();
+  if (cf_handle == nullptr) {
+    RDB_MUTEX_UNLOCK_CHECK(m_mutex);
+    // NO_LINT_DEBUG
+    sql_print_warning(
+        "RocksDB: Cannot mark Column family %s to be dropped, "
+        "because it doesn't exist in cf manager",
+        cf_name.c_str());
+
+    return HA_EXIT_FAILURE;
+  }
+
+  cf_id = cf_handle->GetID();
+  Rdb_cf_scanner scanner(cf_id);
+
+  auto ret = ddl_manager->scan_for_tables(&scanner);
+  if (ret) {
+    RDB_MUTEX_UNLOCK_CHECK(m_mutex);
+    // NO_LINT_DEBUG
+    sql_print_warning(
+        "RocksDB: Cannot mark Column family %s with id %u to be dropped, "
+        "because it is in use",
+        cf_name.c_str(), cf_id);
+    return ret;
+  }
+
+  ret = ddl_manager->find_in_uncommitted_keydef(cf_id);
+  if (ret) {
+    RDB_MUTEX_UNLOCK_CHECK(m_mutex);
+    // NO_LINT_DEBUG
+    sql_print_warning(
+        "RocksDB: Cannot mark Column family %s with id %u to be dropped, "
+        "because it is used by an ongoing add index command",
+        cf_name.c_str(), cf_id);
+    return ret;
+  }
+
+  // We don't call DropColumnFamily on RocksDB here and
+  // we don't delete handle object. Here we mark the column family
+  // as dropped.
+
+  const std::unique_ptr<rocksdb::WriteBatch> wb = dict_manager->begin();
+  rocksdb::WriteBatch *const batch = wb.get();
+
+  dict_manager->add_dropped_cf(batch, cf_id);
+  dict_manager->commit(batch);
+
+  RDB_MUTEX_UNLOCK_CHECK(m_mutex);
+
+  // NO_LINT_DEBUG
+  sql_print_information(
+      "RocksDB: Column family %s with id %u has been marked to be dropped",
+      cf_name.c_str(), cf_id);
+
+  return HA_EXIT_SUCCESS;
+}
+
+int Rdb_cf_manager::create_cf_flags_if_needed(
+    const Rdb_dict_manager *const dict_manager, const uint32 &cf_id,
+    const std::string &cf_name, const bool is_per_partition_cf) {
+  uchar flags =
+      (is_cf_name_reverse(cf_name.c_str()) ? Rdb_key_def::REVERSE_CF_FLAG : 0) |
+      (is_per_partition_cf ? Rdb_key_def::PER_PARTITION_CF_FLAG : 0);
+
+  uint existing_cf_flags;
+  if (dict_manager->get_cf_flags(cf_id, &existing_cf_flags)) {
+    // For the purposes of comparison we'll clear the partitioning bit. The
+    // intent here is to make sure that both partitioned and non-partitioned
+    // tables can refer to the same CF.
+    existing_cf_flags &= ~Rdb_key_def::CF_FLAGS_TO_IGNORE;
+    flags &= ~Rdb_key_def::CF_FLAGS_TO_IGNORE;
+
+    if (existing_cf_flags != flags) {
+      my_error(ER_CF_DIFFERENT, MYF(0), cf_name.c_str(), flags,
+               existing_cf_flags);
+
+      return HA_EXIT_FAILURE;
+    }
+  } else {
+    const std::unique_ptr<rocksdb::WriteBatch> wb = dict_manager->begin();
+    rocksdb::WriteBatch *const batch = wb.get();
+
+    dict_manager->add_cf_flags(batch, cf_id, flags);
+    dict_manager->commit(batch);
+  }
+
+  return HA_EXIT_SUCCESS;
+}
+
 }  // namespace myrocks

inikep/percona-server

Upstream commit ID : fb-mysql-5.6.35/876d9a3fafe36d0cf6b285b1496f1de94eb89739
PS-6869 : Merge fb-prod201907

storage/rocksdb/rdb_cf_manager.cc
@@ -18,16 +18,20 @@
 #pragma implementation  // gcc: Class implementation
 #endif
 
+/* MySQL header files */
+#include "debug_sync.h"
+
 /* This C++ files header file */
+#include "./rdb_threads.h"
 #include "./rdb_cf_manager.h"
 
 /* MyRocks header files */
+#include <string>
+#include <vector>
 #include "./ha_rocksdb.h"
 #include "./ha_rocksdb_proto.h"
 #include "./rdb_datadic.h"
 #include "./rdb_psi.h"
-
-#include <string>
 
 namespace myrocks {
 
@@ -53,17 +57,18 @@ void Rdb_cf_manager::init(
 
   m_cf_options = std::move(cf_options);
 
-  for (auto cfh : *handles) {
-    assert(cfh != nullptr);
-    m_cf_name_map[cfh->GetName()] = cfh;
-    m_cf_id_map[cfh->GetID()] = cfh;
+  for (auto cfh_ptr : *handles) {
+    assert(cfh_ptr != nullptr);
+
+    std::shared_ptr<rocksdb::ColumnFamilyHandle> cfh(cfh_ptr);
+    m_cf_name_map[cfh_ptr->GetName()] = cfh;
+    m_cf_id_map[cfh_ptr->GetID()] = cfh;
   }
 }
 
 void Rdb_cf_manager::cleanup() {
-  for (const auto &it : m_cf_name_map) {
-    delete it.second;
-  }
+  m_cf_name_map.clear();
+  m_cf_id_map.clear();
   mysql_mutex_destroy(&m_mutex);
   m_cf_options = nullptr;
 }
@@ -75,16 +80,16 @@ void Rdb_cf_manager::cleanup() {
   @detail
     See Rdb_cf_manager::get_cf
 */
-rocksdb::ColumnFamilyHandle *Rdb_cf_manager::get_or_create_cf(
+std::shared_ptr<rocksdb::ColumnFamilyHandle> Rdb_cf_manager::get_or_create_cf(
     rocksdb::DB *const rdb, const std::string &cf_name_arg, bool create) {
   assert(rdb != nullptr);
 
-  rocksdb::ColumnFamilyHandle *cf_handle = nullptr;
+  std::shared_ptr<rocksdb::ColumnFamilyHandle> cf_handle;
 
   if (cf_name_arg == PER_INDEX_CF_NAME) {
     // per-index column families is no longer supported.
     my_error(ER_PER_INDEX_CF_DEPRECATED, MYF(0));
-    return nullptr;
+    return cf_handle;
   }
 
   const std::string &cf_name =
@@ -111,14 +116,15 @@ rocksdb::ColumnFamilyHandle *Rdb_cf_manager::get_or_create_cf(
                       "    target_file_size_base=%" PRIu64,
                       opts.target_file_size_base);
 
+      rocksdb::ColumnFamilyHandle *cf_handle_ptr = nullptr;
       const rocksdb::Status s =
-          rdb->CreateColumnFamily(opts, cf_name, &cf_handle);
+          rdb->CreateColumnFamily(opts, cf_name, &cf_handle_ptr);
 
       if (s.ok()) {
-        m_cf_name_map[cf_handle->GetName()] = cf_handle;
-        m_cf_id_map[cf_handle->GetID()] = cf_handle;
-      } else {
-        cf_handle = nullptr;
+        DBUG_ASSERT(cf_handle_ptr != nullptr);
+        cf_handle.reset(cf_handle_ptr);
+        m_cf_name_map[cf_handle_ptr->GetName()] = cf_handle;
+        m_cf_id_map[cf_handle_ptr->GetID()] = cf_handle;
       }
     } else {
       my_error(ER_WRONG_ARGUMENTS, MYF(0),
@@ -137,11 +143,15 @@ rocksdb::ColumnFamilyHandle *Rdb_cf_manager::get_or_create_cf(
 /*
   Find column family by its cf_name.
 */
+std::shared_ptr<rocksdb::ColumnFamilyHandle> Rdb_cf_manager::get_cf(
+    const std::string &cf_name_arg) const {
 
-rocksdb::ColumnFamilyHandle *
-Rdb_cf_manager::get_cf(const std::string &cf_name_arg,
-                       const bool lock_held_by_caller) const {
-  rocksdb::ColumnFamilyHandle *cf_handle;
+  return get_cf(cf_name_arg, false /*lock_held_by_caller*/);
+}
+
+std::shared_ptr<rocksdb::ColumnFamilyHandle> Rdb_cf_manager::get_cf(
+    const std::string &cf_name_arg, const bool lock_held_by_caller) const {
+  std::shared_ptr<rocksdb::ColumnFamilyHandle> cf_handle;
 
   if (!lock_held_by_caller) {
     RDB_MUTEX_LOCK_CHECK(m_mutex);
@@ -149,7 +159,10 @@ Rdb_cf_manager::get_cf(const std::string &cf_name_arg,
   std::string cf_name = cf_name_arg.empty() ? DEFAULT_CF_NAME : cf_name_arg;
 
   const auto it = m_cf_name_map.find(cf_name);
-  cf_handle = (it != m_cf_name_map.end()) ? it->second : nullptr;
+
+  if (it != m_cf_name_map.end()) {
+    cf_handle = it->second;
+  }
 
   if (!cf_handle) {
     LogPluginErrMsg(WARNING_LEVEL, 0, "Column family '%s' not found.",
@@ -163,8 +176,9 @@ Rdb_cf_manager::get_cf(const std::string &cf_name_arg,
   return cf_handle;
 }
 
-rocksdb::ColumnFamilyHandle *Rdb_cf_manager::get_cf(const uint32_t id) const {
-  rocksdb::ColumnFamilyHandle *cf_handle = nullptr;
+std::shared_ptr<rocksdb::ColumnFamilyHandle> Rdb_cf_manager::get_cf(
+    const uint32_t id) const {
+  std::shared_ptr<rocksdb::ColumnFamilyHandle> cf_handle;
 
   RDB_MUTEX_LOCK_CHECK(m_mutex);
   const auto it = m_cf_id_map.find(id);
@@ -186,9 +200,9 @@ std::vector<std::string> Rdb_cf_manager::get_cf_names(void) const {
   return names;
 }
 
-std::vector<rocksdb::ColumnFamilyHandle *> Rdb_cf_manager::get_all_cf(
-    void) const {
-  std::vector<rocksdb::ColumnFamilyHandle *> list;
+std::vector<std::shared_ptr<rocksdb::ColumnFamilyHandle>>
+Rdb_cf_manager::get_all_cf(void) const {
+  std::vector<std::shared_ptr<rocksdb::ColumnFamilyHandle>> list;
 
   RDB_MUTEX_LOCK_CHECK(m_mutex);
 
@@ -202,66 +216,67 @@ std::vector<rocksdb::ColumnFamilyHandle *> Rdb_cf_manager::get_all_cf(
   return list;
 }
 
-struct Rdb_cf_scanner : public Rdb_tables_scanner {
-  uint32_t m_cf_id;
-  int m_is_cf_used;
-
-  explicit Rdb_cf_scanner(uint32_t cf_id)
-      : m_cf_id(cf_id), m_is_cf_used(false) {}
-
-  int add_table(Rdb_tbl_def *tdef) override {
-    DBUG_ASSERT(tdef != nullptr);
-
-    for (uint i = 0; i < tdef->m_key_count; i++) {
-      const Rdb_key_def &kd = *tdef->m_key_descr_arr[i];
-
-      if (kd.get_cf()->GetID() == m_cf_id) {
-        m_is_cf_used = true;
-        return HA_EXIT_SUCCESS;
-      }
-    }
-    return HA_EXIT_SUCCESS;
-  }
-};
-
-int Rdb_cf_manager::drop_cf(const std::string &cf_name) {
-  auto ddl_manager = rdb_get_ddl_manager();
-  uint32_t cf_id = 0;
-
-  if (cf_name == DEFAULT_SYSTEM_CF_NAME) {
-    return HA_EXIT_FAILURE;
-  }
-
+int Rdb_cf_manager::remove_dropped_cf(Rdb_dict_manager *const dict_manager,
+                                      rocksdb::TransactionDB *const rdb,
+                                      const uint32 &cf_id) {
+  dict_manager->assert_lock_held();
   RDB_MUTEX_LOCK_CHECK(m_mutex);
-  auto cf_handle = get_cf(cf_name, true /* lock_held_by_caller */);
-  if (cf_handle == nullptr) {
-    RDB_MUTEX_UNLOCK_CHECK(m_mutex);
-    return HA_EXIT_SUCCESS;
-  }
+  const std::unique_ptr<rocksdb::WriteBatch> wb = dict_manager->begin();
+  rocksdb::WriteBatch *const batch = wb.get();
 
-  cf_id = cf_handle->GetID();
-  Rdb_cf_scanner scanner(cf_id);
-
-  auto ret = ddl_manager->scan_for_tables(&scanner);
-  if (ret) {
+  const auto it = m_cf_id_map.find(cf_id);
+  if (it == m_cf_id_map.end()) {
+    dict_manager->delete_dropped_cf_and_flags(batch, cf_id);
+    dict_manager->commit(batch);
     RDB_MUTEX_UNLOCK_CHECK(m_mutex);
-    return ret;
-  }
 
-  if (scanner.m_is_cf_used) {
-    // column family is used by existing key
-    RDB_MUTEX_UNLOCK_CHECK(m_mutex);
+    // NO_LINT_DEBUG
+    sql_print_warning(
+        "RocksDB: Column family with id %u is marked as dropped, "
+        "but doesn't exist in cf manager",
+        cf_id);
+
     return HA_EXIT_FAILURE;
   }
 
-  auto rdb = rdb_get_rocksdb_db();
-  auto status = rdb->DropColumnFamily(cf_handle);
-  if (!status.ok()) {
+  auto cf_handle = it->second.get();
+  const std::string cf_name = cf_handle->GetName();
+
+  if (!dict_manager->get_dropped_cf(cf_id)) {
     RDB_MUTEX_UNLOCK_CHECK(m_mutex);
+    // NO_LINT_DEBUG
+    sql_print_warning(
+        "RocksDB: Column family %s with id %u is not in "
+        "the list of cf ids to be dropped",
+        cf_name.c_str(), cf_id);
+    return HA_EXIT_FAILURE;
+  }
+
+  auto status = rdb->DropColumnFamily(cf_handle);
+
+  if (!status.ok()) {
+    dict_manager->delete_dropped_cf(batch, cf_id);
+    dict_manager->commit(batch);
+    RDB_MUTEX_UNLOCK_CHECK(m_mutex);
+
+    // NO_LINT_DEBUG
+    sql_print_error(
+        "RocksDB: Dropping column family %s with id %u on RocksDB failed",
+        cf_name.c_str(), cf_id);
+
     return ha_rocksdb::rdb_error_to_mysql(status);
   }
 
-  delete cf_handle;
+  DBUG_EXECUTE_IF("rocksdb_remove_dropped_cf", {
+    THD *thd = new THD();
+    thd->thread_stack = reinterpret_cast<char *>(&(thd));
+    thd->store_globals();
+    static constexpr char act[] =
+        "now signal ready_to_restart_during_drop_cf";
+    DBUG_ASSERT(!debug_sync_set_action(thd, STRING_WITH_LEN(act)));
+    thd->restore_globals();
+    delete thd;
+  });
 
   auto id_iter = m_cf_id_map.find(cf_id);
   DBUG_ASSERT(id_iter != m_cf_id_map.end());
@@ -271,8 +286,144 @@ int Rdb_cf_manager::drop_cf(const std::string &cf_name) {
   DBUG_ASSERT(name_iter != m_cf_name_map.end());
   m_cf_name_map.erase(name_iter);
 
+  dict_manager->delete_dropped_cf_and_flags(batch, cf_id);
+
+  dict_manager->commit(batch);
   RDB_MUTEX_UNLOCK_CHECK(m_mutex);
+
+  // NO_LINT_DEBUG
+  sql_print_information(
+      "RocksDB: Column family %s with id %u has been dropped successfully",
+      cf_name.c_str(), cf_id);
 
   return HA_EXIT_SUCCESS;
 }
-} // namespace myrocks
+
+namespace {
+struct Rdb_cf_scanner : public Rdb_tables_scanner {
+  uint32_t m_cf_id;
+
+  explicit Rdb_cf_scanner(uint32_t cf_id) : m_cf_id(cf_id) {}
+
+  int add_table(Rdb_tbl_def *tdef) override {
+    DBUG_ASSERT(tdef != nullptr);
+
+    for (uint i = 0; i < tdef->m_key_count; i++) {
+      const Rdb_key_def &kd = *tdef->m_key_descr_arr[i];
+
+      if (kd.get_cf()->GetID() == m_cf_id) {
+        return HA_EXIT_FAILURE;
+      }
+    }
+    return HA_EXIT_SUCCESS;
+  }
+};
+
+}  // namespace
+
+int Rdb_cf_manager::drop_cf(Rdb_ddl_manager *const ddl_manager,
+                            Rdb_dict_manager *const dict_manager,
+                            const std::string &cf_name) {
+  dict_manager->assert_lock_held();
+  uint32_t cf_id = 0;
+
+  if (cf_name == DEFAULT_SYSTEM_CF_NAME || cf_name == DEFAULT_CF_NAME ||
+      cf_name.empty()) {
+    return HA_EXIT_FAILURE;
+  }
+
+  // We should have already acquired dict manager lock. The order
+  // of lock acquisition here is:
+  //  dict_manager -> cf_manager -> ddl_manager
+
+  RDB_MUTEX_LOCK_CHECK(m_mutex);
+  auto cf_handle = get_cf(cf_name, true /* lock_held_by_caller */).get();
+  if (cf_handle == nullptr) {
+    RDB_MUTEX_UNLOCK_CHECK(m_mutex);
+    // NO_LINT_DEBUG
+    sql_print_warning(
+        "RocksDB: Cannot mark Column family %s to be dropped, "
+        "because it doesn't exist in cf manager",
+        cf_name.c_str());
+
+    return HA_EXIT_FAILURE;
+  }
+
+  cf_id = cf_handle->GetID();
+  Rdb_cf_scanner scanner(cf_id);
+
+  auto ret = ddl_manager->scan_for_tables(&scanner);
+  if (ret) {
+    RDB_MUTEX_UNLOCK_CHECK(m_mutex);
+    // NO_LINT_DEBUG
+    sql_print_warning(
+        "RocksDB: Cannot mark Column family %s with id %u to be dropped, "
+        "because it is in use",
+        cf_name.c_str(), cf_id);
+    return ret;
+  }
+
+  ret = ddl_manager->find_in_uncommitted_keydef(cf_id);
+  if (ret) {
+    RDB_MUTEX_UNLOCK_CHECK(m_mutex);
+    // NO_LINT_DEBUG
+    sql_print_warning(
+        "RocksDB: Cannot mark Column family %s with id %u to be dropped, "
+        "because it is used by an ongoing add index command",
+        cf_name.c_str(), cf_id);
+    return ret;
+  }
+
+  // We don't call DropColumnFamily on RocksDB here and
+  // we don't delete handle object. Here we mark the column family
+  // as dropped.
+
+  const std::unique_ptr<rocksdb::WriteBatch> wb = dict_manager->begin();
+  rocksdb::WriteBatch *const batch = wb.get();
+
+  dict_manager->add_dropped_cf(batch, cf_id);
+  dict_manager->commit(batch);
+
+  RDB_MUTEX_UNLOCK_CHECK(m_mutex);
+
+  // NO_LINT_DEBUG
+  sql_print_information(
+      "RocksDB: Column family %s with id %u has been marked to be dropped",
+      cf_name.c_str(), cf_id);
+
+  return HA_EXIT_SUCCESS;
+}
+
+int Rdb_cf_manager::create_cf_flags_if_needed(
+    const Rdb_dict_manager *const dict_manager, const uint32 &cf_id,
+    const std::string &cf_name, const bool is_per_partition_cf) {
+  uchar flags =
+      (is_cf_name_reverse(cf_name.c_str()) ? Rdb_key_def::REVERSE_CF_FLAG : 0) |
+      (is_per_partition_cf ? Rdb_key_def::PER_PARTITION_CF_FLAG : 0);
+
+  uint existing_cf_flags;
+  if (dict_manager->get_cf_flags(cf_id, &existing_cf_flags)) {
+    // For the purposes of comparison we'll clear the partitioning bit. The
+    // intent here is to make sure that both partitioned and non-partitioned
+    // tables can refer to the same CF.
+    existing_cf_flags &= ~Rdb_key_def::CF_FLAGS_TO_IGNORE;
+    flags &= ~Rdb_key_def::CF_FLAGS_TO_IGNORE;
+
+    if (existing_cf_flags != flags) {
+      my_error(ER_CF_DIFFERENT, MYF(0), cf_name.c_str(), flags,
+               existing_cf_flags);
+
+      return HA_EXIT_FAILURE;
+    }
+  } else {
+    const std::unique_ptr<rocksdb::WriteBatch> wb = dict_manager->begin();
+    rocksdb::WriteBatch *const batch = wb.get();
+
+    dict_manager->add_cf_flags(batch, cf_id, flags);
+    dict_manager->commit(batch);
+  }
+
+  return HA_EXIT_SUCCESS;
+}
+
+}  // namespace myrocks