afc269a813923d27fe092e29e6756c426c55ee39
Przemyslaw Skibinski <przemyslaw.skibinski@percona.com>
2023-08-15 11:27:30 +0200

    Revert "Improve ZenFS build"
    This reverts commit 08f28546c8876b306a96c39c462e20aa0a01940d.

--- build-ps/percona-server-8.0_builder.sh
+++ build-ps/percona-server-8.0_builder.sh
@@ -25,8 +25,6 @@ Usage: $0 [OPTIONS]
         --perconaft_branch          Branch for PerconaFT
 	--tokubackup_repo           TokuBackup repo (The TokuDB storage has no longer supported since 8.0.28)
         --tokubackup_branch         Branch for TokuBackup
-        --zenfs_repo                ZenFS repo
-        --zenfs_branch              Branch for ZenFS
         --rpm_release               RPM version( default = 1)
         --deb_release               DEB version( default = 1)
         --debug                     Build debug tarball
@@ -67,10 +65,8 @@ parse_arguments() {
             --build_tokudb_tokubackup=*) BUILD_TOKUDB_TOKUBACKUP="$val" ;;
             --perconaft_branch=*) PERCONAFT_BRANCH="$val" ;;
             --tokubackup_branch=*)      TOKUBACKUP_BRANCH="$val" ;;
-            --zenfs_branch=*)      ZENFS_BRANCH="$val" ;;
             --perconaft_repo=*) PERCONAFT_REPO="$val" ;;
             --tokubackup_repo=*) TOKUBACKUP_REPO="$val" ;;
-            --zenfs_repo=*) ZENFS_REPO="$val" ;;
             --rpm_release=*) RPM_RELEASE="$val" ;;
             --deb_release=*) DEB_RELEASE="$val" ;;
             --debug=*) DEBUG="$val" ;;
@@ -207,8 +203,6 @@ get_sources(){
     echo "PERCONAFT_BRANCH=${PERCONAFT_BRANCH}" >> ../percona-server-8.0.properties
     echo "TOKUBACKUP_REPO=${TOKUBACKUP_REPO}" >> ../percona-server-8.0.properties
     echo "TOKUBACKUP_BRANCH=${TOKUBACKUP_BRANCH}" >> ../percona-server-8.0.properties
-    echo "ZENFS_REPO=${ZENFS_REPO}" >> ../percona-server-8.0.properties
-    echo "ZENFS_BRANCH=${ZENFS_BRANCH}" >> ../percona-server-8.0.properties
     export TOKUDB_VERSION=${MYSQL_VERSION_MAJOR}.${MYSQL_VERSION_MINOR}.${MYSQL_VERSION_PATCH}${MYSQL_VERSION_EXTRA}
     echo "TOKUDB_VERSION=${MYSQL_VERSION_MAJOR}.${MYSQL_VERSION_MINOR}.${MYSQL_VERSION_PATCH}${MYSQL_VERSION_EXTRA}" >> ../percona-server-8.0.properties
     BOOST_PACKAGE_NAME=$(cat cmake/boost.cmake|grep "SET(BOOST_PACKAGE_NAME"|awk -F '"' '{print $2}')
@@ -273,17 +267,6 @@ get_sources(){
         fi
     fi
     #
-
-    if [ ! ${ZENFS_REPO} = 0 ]; then
-        rm -rf ${WORKDIR}/percona-server/storage/rocksdb/rocksdb_plugins/zenfs
-        git clone ${ZENFS_REPO} ${WORKDIR}/percona-server/storage/rocksdb/rocksdb_plugins/zenfs
-    fi
-    if [ ! ${ZENFS_BRANCH} = 0 ]; then
-        cd ${WORKDIR}/percona-server/storage/rocksdb/rocksdb_plugins/zenfs
-        git checkout ${ZENFS_BRANCH}
-        cd -
-    fi
-
     git submodule update
     cd storage/rocksdb/rocksdb_plugins/zenfs
     ZEN_VER=$(git describe --abbrev=7 --dirty)
