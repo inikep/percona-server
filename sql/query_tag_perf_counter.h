facebook/mysql-5.6

Implemented measuring CPU usage for queries instrumented with query attributes. Added system table QUERY_PERF_COUNTER to output results of measurements.
Summary:
Implemented measuring CPU usage for queries instrumented with query attributes. Added system table QUERY_PERF_COUNTER to output results of measurements.
Reference patch: https://github.com/facebook/mysql-5.6/commit/5832232
Reference patch: https://github.com/facebook/mysql-5.6/commit/caf9fe3
Reference patch: https://github.com/facebook/mysql-5.6/commit/453353b

Reviewed By: asandryh

Differential Revision: D9212758

fbshipit-source-id: f4890b59f3f

sql/query_tag_perf_counter.h
@@ -1,0 +1,33 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#ifndef QUERY_TAG_PERF_COUNTER_H_
+#define QUERY_TAG_PERF_COUNTER_H_
+
+#include <ctime>
+#include <string>
+
+class Item;
+class THD;
+struct TABLE_LIST;
+struct ST_FIELD_INFO;
+
+namespace qutils {
+
+extern ST_FIELD_INFO query_tag_perf_fields_info[];
+extern int fill_query_tag_perf_counter(THD *thd, TABLE_LIST *tables,
+                                       Item *cond);
+
+class query_tag_perf_counter {
+ public:
+  query_tag_perf_counter(THD *thd);
+  ~query_tag_perf_counter();
+
+ private:
+  struct timespec starttime;  // query start timestamp
+  bool started;               // query_info attribute detected
+  THD *thd;                   // reference to outer THD
+};
+
+}  // namespace qutils
+
+#endif /* QUERY_TAG_PERF_COUNTER_H_ */

inikep/percona-server

sql/query_tag_perf_counter.h
@@ -1,0 +1,33 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+#ifndef QUERY_TAG_PERF_COUNTER_H_
+#define QUERY_TAG_PERF_COUNTER_H_
+
+#include <ctime>
+#include <string>
+
+class Item;
+class THD;
+class Table_ref;
+struct ST_FIELD_INFO;
+
+namespace qutils {
+
+extern ST_FIELD_INFO query_tag_perf_fields_info[];
+extern int fill_query_tag_perf_counter(THD *thd, Table_ref *tables,
+                                       Item *cond);
+
+class query_tag_perf_counter {
+ public:
+  query_tag_perf_counter(THD *thd);
+  ~query_tag_perf_counter();
+
+ private:
+  struct timespec starttime;  // query start timestamp
+  bool started;               // query_info attribute detected
+  THD *thd;                   // reference to outer THD
+};
+
+}  // namespace qutils
+
+#endif /* QUERY_TAG_PERF_COUNTER_H_ */