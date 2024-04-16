inikep/mysql-5.6

Refactor regex code to handle regexes in table lists easier [non-RocksDB part]
Summary: I need to add another variable that handles regular expressions.  The current code is not shared as much as it could be and doesn't handle invalid regex patterns (i.e. no ending bracket or trailing escape character).

Differential Revision: D61551

sql/handler.cc
@@ -8838,3 +8838,66 @@ bool ha_check_reserved_db_name(const char *name) {
 bool is_index_access_error(int error) {
   return (error != HA_ERR_END_OF_FILE && error != HA_ERR_KEY_NOT_FOUND);
 }
+
+/*
+  Set the patterns string.  If there are invalid regex patterns they will
+  be stored in m_bad_patterns and the result will be false, otherwise the
+  result will be true.
+*/
+bool Regex_list_handler::set_patterns(const std::string &pattern_str) {
+  bool pattern_valid = true;
+
+  // Create a normalized version of the pattern string with all delimiters
+  // replaced by the '|' character
+  std::string norm_pattern = pattern_str;
+  std::replace(norm_pattern.begin(), norm_pattern.end(), m_delimiter, '|');
+
+  // Make sure no one else is accessing the list while we are changing it.
+  mysql_rwlock_wrlock(&m_rwlock);
+
+  // Clear out any old error information
+  m_bad_pattern_str.clear();
+
+  try {
+    // Replace all delimiters with the '|' operator and create the regex
+    // Note that this means the delimiter can not be part of a regular
+    // expression.  This is currently not a problem as we are using the comma
+    // character as a delimiter and commas are not valid in table names.
+    m_pattern.reset(new std::regex(norm_pattern));
+  } catch (const std::regex_error &e) {
+    // This pattern is invalid.
+    pattern_valid = false;
+
+    // Put the bad pattern into a member variable so it can be retrieved later.
+    m_bad_pattern_str = pattern_str;
+  }
+
+  // Release the lock
+  mysql_rwlock_unlock(&m_rwlock);
+
+  return pattern_valid;
+}
+
+bool Regex_list_handler::matches(const std::string &str) const {
+  DBUG_ASSERT(m_pattern != nullptr);
+
+  // Make sure no one else changes the list while we are accessing it.
+  mysql_rwlock_rdlock(&m_rwlock);
+
+  // See if the table name matches the regex we have created
+  bool found = std::regex_match(str, *m_pattern);
+
+  // Release the lock
+  mysql_rwlock_unlock(&m_rwlock);
+
+  return found;
+}
+
+void warn_about_bad_patterns(const Regex_list_handler *regex_list_handler,
+                             const char *name) {
+  // There was some invalid regular expression data in the patterns supplied
+
+  // NO_LINT_DEBUG
+  sql_print_warning("Invalid pattern in %s: %s", name,
+                    regex_list_handler->bad_pattern().c_str());
+}

inikep/percona-server

sql/handler.cc
@@ -8880,3 +8880,66 @@ Xa_state_list::instantiation_tuple Xa_state_list::new_instance() {
       std::move(mem_root), std::move(map_alloc), std::move(xid_map),
       std::move(xa_list));
 }
+
+/*
+  Set the patterns string.  If there are invalid regex patterns they will
+  be stored in m_bad_patterns and the result will be false, otherwise the
+  result will be true.
+*/
+bool Regex_list_handler::set_patterns(const std::string &pattern_str) {
+  bool pattern_valid = true;
+
+  // Create a normalized version of the pattern string with all delimiters
+  // replaced by the '|' character
+  std::string norm_pattern = pattern_str;
+  std::replace(norm_pattern.begin(), norm_pattern.end(), m_delimiter, '|');
+
+  // Make sure no one else is accessing the list while we are changing it.
+  mysql_rwlock_wrlock(&m_rwlock);
+
+  // Clear out any old error information
+  m_bad_pattern_str.clear();
+
+  try {
+    // Replace all delimiters with the '|' operator and create the regex
+    // Note that this means the delimiter can not be part of a regular
+    // expression.  This is currently not a problem as we are using the comma
+    // character as a delimiter and commas are not valid in table names.
+    m_pattern.reset(new std::regex(norm_pattern));
+  } catch (const std::regex_error &e) {
+    // This pattern is invalid.
+    pattern_valid = false;
+
+    // Put the bad pattern into a member variable so it can be retrieved later.
+    m_bad_pattern_str = pattern_str;
+  }
+
+  // Release the lock
+  mysql_rwlock_unlock(&m_rwlock);
+
+  return pattern_valid;
+}
+
+bool Regex_list_handler::matches(const std::string &str) const {
+  assert(m_pattern != nullptr);
+
+  // Make sure no one else changes the list while we are accessing it.
+  mysql_rwlock_rdlock(&m_rwlock);
+
+  // See if the table name matches the regex we have created
+  bool found = std::regex_match(str, *m_pattern);
+
+  // Release the lock
+  mysql_rwlock_unlock(&m_rwlock);
+
+  return found;
+}
+
+void warn_about_bad_patterns(const Regex_list_handler *regex_list_handler,
+                             const char *name) {
+  // There was some invalid regular expression data in the patterns supplied
+
+  // NO_LINT_DEBUG
+  sql_print_warning("Invalid pattern in %s: %s", name,
+                    regex_list_handler->bad_pattern().c_str());
+}