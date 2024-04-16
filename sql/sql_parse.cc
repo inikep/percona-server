inikep/mysql-5.6

Bypass ACL DDLs in gap lock error check [non-RocksDB part]
Summary:
In MySQL 8.0, auto commit is turned off for both ACL DDL and
DDL queries to avoid unnecessary commits in DD. For DDLs, auto
commit can be turned back on in DD, but not for ACL queries if
they don't go into DD. As a result, the ACL query is treated as
multi-statement query when it comes to checking the gap lock hence
is prohibited. This commit bypasses ACL queries when checking the
gap lock.

Differential Revision: D18920694

sql/sql_parse.cc
@@ -1080,6 +1080,19 @@ bool is_update_query(enum enum_sql_command command) {
   return (sql_command_flags[command] & CF_CHANGES_DATA) != 0;
 }
 
+bool is_acl_ddl_query(enum enum_sql_command command) {
+  DBUG_ASSERT(command >= 0 && command <= SQLCOM_END);
+  return (command == SQLCOM_SET_ROLE ||
+          command == SQLCOM_ALTER_USER_DEFAULT_ROLE ||
+          command == SQLCOM_CREATE_ROLE || command == SQLCOM_DROP_ROLE ||
+          command == SQLCOM_GRANT_ROLE || command == SQLCOM_ALTER_USER ||
+          command == SQLCOM_GRANT || command == SQLCOM_REVOKE ||
+          command == SQLCOM_REVOKE_ALL || command == SQLCOM_REVOKE_ROLE ||
+          command == SQLCOM_CREATE_USER || command == SQLCOM_DROP_USER ||
+          command == SQLCOM_RENAME_USER || command == SQLCOM_SHOW_GRANTS ||
+          command == SQLCOM_SET_PASSWORD);
+}
+
 bool is_explainable_query(enum enum_sql_command command) {
   DBUG_ASSERT(command >= 0 && command <= SQLCOM_END);
   return (sql_command_flags[command] & CF_CAN_BE_EXPLAINED) != 0;

inikep/percona-server

sql/sql_parse.cc
@@ -1169,6 +1169,19 @@ bool is_update_query(enum enum_sql_command command) {
   return (sql_command_flags[command] & CF_CHANGES_DATA) != 0;
 }
 
+bool is_acl_ddl_query(enum enum_sql_command command) {
+  assert(command >= 0 && command <= SQLCOM_END);
+  return (command == SQLCOM_SET_ROLE ||
+          command == SQLCOM_ALTER_USER_DEFAULT_ROLE ||
+          command == SQLCOM_CREATE_ROLE || command == SQLCOM_DROP_ROLE ||
+          command == SQLCOM_GRANT_ROLE || command == SQLCOM_ALTER_USER ||
+          command == SQLCOM_GRANT || command == SQLCOM_REVOKE ||
+          command == SQLCOM_REVOKE_ALL || command == SQLCOM_REVOKE_ROLE ||
+          command == SQLCOM_CREATE_USER || command == SQLCOM_DROP_USER ||
+          command == SQLCOM_RENAME_USER || command == SQLCOM_SHOW_GRANTS ||
+          command == SQLCOM_SET_PASSWORD);
+}
+
 bool is_explainable_query(enum enum_sql_command command) {
   assert(command >= 0 && command <= SQLCOM_END);
   return (sql_command_flags[command] & CF_CAN_BE_EXPLAINED) != 0;