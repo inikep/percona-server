inikep/mysql-5.6

Make dual engine recovery work correctly [non-RocksDB part]
Summary:
In MyRocks8.0, we need both InnoDB and RocksDB engines running since
data dictionary and temp tables use InnoDB. Binlog positions are written
separately into each engine during trx commit time. At recovery, the
binlog position should be set to the minimum positions of all engines so
that when binlog is ahead of storage engine, the slave doesn't miss any
transactions from both storage engines. Also, max gtid is set to the max
across all engines in order for idempodent recovery to work.

Differential Revision: D18694795

sql/binlog.cc
@@ -5950,6 +5950,27 @@ uint split_file_name_and_gtid_set_length(char *file_name_and_gtid_set_length) {
   return 0;
 }
 
+bool is_binlog_advanced(const char *b1, my_off_t p1, const char *b2,
+                        my_off_t p2) {
+  if (!b1 || !b1[0]) {
+    return (b2 && b2[0] && p2 != ULLONG_MAX);
+  }
+
+  if (!b2 || !b2[0]) {
+    return false;
+  }
+
+  DBUG_ASSERT(p1 != ULLONG_MAX && p2 != ULLONG_MAX);
+
+  int cmp = strlen(b1) - strlen(b2);
+
+  if (cmp) {
+    return (cmp < 0);
+  }
+
+  return (strcmp(b1, b2) < 0 || (strcmp(b1, b2) == 0 && p1 < p2));
+}
+
 /**
   Find the position in the log-index-file for the given log name.
 

inikep/percona-server

sql/binlog.cc
@@ -5647,6 +5647,27 @@ uint split_file_name_and_gtid_set_length(char *file_name_and_gtid_set_length) {
   return 0;
 }
 
+bool is_binlog_advanced(const char *b1, my_off_t p1, const char *b2,
+                        my_off_t p2) {
+  if (!b1 || !b1[0]) {
+    return (b2 && b2[0] && p2 != ULLONG_MAX);
+  }
+
+  if (!b2 || !b2[0]) {
+    return false;
+  }
+
+  assert(p1 != ULLONG_MAX && p2 != ULLONG_MAX);
+
+  int cmp = strlen(b1) - strlen(b2);
+
+  if (cmp) {
+    return (cmp < 0);
+  }
+
+  return (strcmp(b1, b2) < 0 || (strcmp(b1, b2) == 0 && p1 < p2));
+}
+
 /**
   Find the position in the log-index-file for the given log name.
 