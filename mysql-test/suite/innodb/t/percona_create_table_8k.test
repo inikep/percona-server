mysql/mysql-server

commit 6d587a60738842b1f7e5e5b0839acd62db6b9d43

Bug #30776132: MAKE FILESORT KEYS CONSISTENT BETWEEN FIELDS AND ITEMS
Since the dawn of time, filesort has had different paths for making sort keys
for fields and items. There's no good reason to keep carry this distinction
forward anymore, so remove the special handling of Field and just sort Items
everywhere, making filesort somewhat simpler. There are some special legacy
cases with sorting enums etc. where we still need to look into Item_field and
look at the actual underlying file type, but they are fairly localized and now
much more visible.

There are some edge cases where keys used to be shorter, e.g. all integers
will now get 64-bit sort keys instead of 8-bit, 16-bit, etc.. Also, some
EXPLAIN output changes, as we also no longer have specialized formatting of
Field.

WEIGHT_STRING() now is able to get the actual weight strings for more kinds
of items, in particular integers.

Inadvertently, we fix a bug that was only half-visible in the tests;
common-tests.inc contain a query whose results were not properly deduplicated
before, but are now, and while this was hidden by LIMIT/OFFSET, it affects
which rows are visible in the result set.

sysbench sorting tests (ranges and distinct ranges) show strong improvements
at high thread counts; as much as +17%. Lower thread counts are neutral or
in the noise.

Unfortunately, replication started using Field::make_sort_key() in 8.0, and
cannot easily change it back, as they need exact byte-by-byte matches of the
data. Fixing this would enable removing 500+ lines of complicated sort key
code.

Change-Id: Ieb5038e8afbea19a5f0e0e7f83b3177cb3176e84

mysql-test/r/order_by_sortkey.result
@@ -152,7 +152,7 @@ f0	f1	f2
 9901	0	0
 SHOW SESSION STATUS LIKE 'Sort%';
 Variable_name	Value
-Sort_merge_passes	99
+Sort_merge_passes	102
 Sort_range	0
 Sort_rows	100
 Sort_scan	1

mysql-test/r/compress.result
@@ -610,7 +610,6 @@ companynr	fld3	count(*)
 SET BIG_TABLES=0;
 select distinct fld3,repeat("a",length(fld3)),count(*) from t2 group by companynr,fld3 limit 100,10;
 fld3	repeat("a",length(fld3))	count(*)
-babysitting	aaaaaaaaaaa	1
 Baird	aaaaa	1
 balled	aaaaaa	1
 ballgown	aaaaaaaa	1
@@ -620,6 +619,7 @@ Barry	aaaaa	1
 batting	aaaaaaa	1
 beaner	aaaaaa	1
 beasts	aaaaaa	1
+beaters	aaaaaaa	1
 select distinct companynr,rtrim(space(512+companynr)) from t3 order by 1,2;
 companynr	rtrim(space(512+companynr))
 37	
@@ -2873,7 +2873,6 @@ companynr	fld3	count(*)
 SET BIG_TABLES=0;
 select distinct fld3,repeat("a",length(fld3)),count(*) from t2 group by companynr,fld3 limit 100,10;
 fld3	repeat("a",length(fld3))	count(*)
-babysitting	aaaaaaaaaaa	1
 Baird	aaaaa	1
 balled	aaaaaa	1
 ballgown	aaaaaaaa	1
@@ -2883,6 +2882,7 @@ Barry	aaaaa	1
 batting	aaaaaaa	1
 beaner	aaaaaa	1
 beasts	aaaaaa	1
+beaters	aaaaaaa	1
 select distinct companynr,rtrim(space(512+companynr)) from t3 order by 1,2;
 companynr	rtrim(space(512+companynr))
 37	

mysql-test/r/ctype_latin2_ch_myisam.result
@@ -53,5 +53,5 @@ t1	CREATE TABLE `t1` (
 INSERT INTO t1 (ID,Post) VALUES (00041,'');
 SELECT ID, Post, HEX(WEIGHT_STRING(Post)) FROM t1;
 ID	Post	HEX(WEIGHT_STRING(Post))
-00041		01010100
+00041		8000000000000001
 DROP TABLE t1;

mysql-test/r/distinct.result
@@ -1004,7 +1004,7 @@ c1	c2	COUNT(*)
 4	4	1
 EXPLAIN FORMAT=tree SELECT DISTINCT c2 FROM t1 GROUP BY c1 HAVING COUNT(*) > 1;
 EXPLAIN
--> Sort with duplicate removal: <temporary>.c2
+-> Sort with duplicate removal: t1.c2
     -> Filter: (count(0) > 1)
         -> Table scan on <temporary>
             -> Aggregate using temporary table
@@ -1254,7 +1254,7 @@ INSERT INTO t1 SELECT RAND() FROM t1;
 INSERT INTO t1 SELECT RAND() FROM t1;
 EXPLAIN FORMAT=tree SELECT DISTINCT COUNT(*) AS num FROM t1 GROUP BY f HAVING num=1;
 EXPLAIN
--> Sort with duplicate removal: <temporary>.num
+-> Sort with duplicate removal: num
     -> Filter: (num = 1)
         -> Table scan on <temporary>
             -> Aggregate using temporary table
@@ -1285,7 +1285,7 @@ Table	Op	Msg_type	Msg_text
 test.t1	analyze	status	OK
 EXPLAIN FORMAT=tree SELECT DISTINCT SQL_BIG_RESULT a FROM t1 GROUP BY a,b ORDER BY a DESC;
 EXPLAIN
--> Sort with duplicate removal: <temporary>.a DESC
+-> Sort with duplicate removal: t1.a DESC
     -> Stream results
         -> Group (no aggregates)
             -> Sort: t1.a DESC, t1.b  (cost=0.80 rows=3)

mysql-test/r/explain_tree.result
@@ -232,7 +232,7 @@ EXPLAIN
     -> Table scan on t1  (cost=0.35 rows=1)
     -> Select #2 (subquery in condition; dependent)
         -> Group aggregate: group_concat(t.b separator ',')
-            -> Sort row IDs: <temporary>.a
+            -> Sort row IDs: t1.a
                 -> Table scan on <temporary>
                     -> Temporary table
                         -> Table scan on t  (cost=0.35 rows=1)
@@ -382,7 +382,7 @@ Table	Op	Msg_type	Msg_text
 test.t1	analyze	status	OK
 EXPLAIN FORMAT=tree SELECT ( SELECT f1 FROM t1 AS inner_t1 WHERE inner_t1.f1 > t1.f1 LIMIT 1 ) AS tmp1 FROM t1 ORDER BY tmp1;
 EXPLAIN
--> Sort: <temporary>.tmp1
+-> Sort: `<temporary>`.tmp1
     -> Stream results
         -> Index scan on t1 using PRIMARY  (cost=0.55 rows=3)
 -> Select #2 (subquery in projection; dependent)
@@ -707,7 +707,7 @@ test.t1	analyze	status	OK
 EXPLAIN ANALYZE SELECT * FROM t1 AS a JOIN t1 AS b ON a.a=b.b ORDER BY a.b+b.a LIMIT 3;
 EXPLAIN
 -> Limit: 3 row(s)  (actual time=N.NNN..N.NNN rows=1 loops=1)
-    -> Sort: <temporary>.(a.b + b.a), limit input to 3 row(s) per chunk  (actual time=N.NNN..N.NNN rows=1 loops=1)
+    -> Sort: `<temporary>`.`(a.b + b.a)`, limit input to 3 row(s) per chunk  (actual time=N.NNN..N.NNN rows=1 loops=1)
         -> Stream results  (actual time=N.NNN..N.NNN rows=1 loops=1)
             -> Nested loop inner join  (cost=1.60 rows=3) (actual time=N.NNN..N.NNN rows=1 loops=1)
                 -> Filter: (b.b is not null)  (cost=0.55 rows=3) (actual time=N.NNN..N.NNN rows=3 loops=1)

mysql-test/r/filesort_debug.result
@@ -135,8 +135,8 @@ CREATE TABLE t1(a DATE);
 INSERT INTO t1 VALUES('1000-01-01'), ('2017-10-31');
 SELECT HEX(WEIGHT_STRING(a)) FROM t1;
 HEX(WEIGHT_STRING(a))
-07D021
-0FC35F
+8CB2420000000000
+999DFE0000000000
 SELECT HEX(WEIGHT_STRING(a COLLATE utf8mb4_0900_ai_ci, 3, 3, 0xC0)) FROM t1;
 HEX(WEIGHT_STRING(a COLLATE utf8mb4_0900_ai_ci, 3, 3, 0xC0))
 1C3E1C

mysql-test/r/group_by.result
@@ -3912,7 +3912,7 @@ Table	Op	Msg_type	Msg_text
 test.t1	analyze	status	OK
 EXPLAIN FORMAT=tree SELECT f1, f1 + 1, COUNT(DISTINCT f2) AS x FROM t1 GROUP BY f1 ORDER BY x;
 EXPLAIN
--> Sort: <temporary>.x
+-> Sort: x
     -> Stream results
         -> Group aggregate: count(distinct t1.f2)
             -> Index scan on t1 using k1  (cost=0.85 rows=6)

mysql-test/r/hash_join.result
@@ -13,7 +13,7 @@ test.t2	analyze	status	OK
 EXPLAIN FORMAT=tree
 SELECT t1.col1, t2.col1 FROM t1 JOIN t2 ON t1.col1 = t2.col1 ORDER BY t1.col1;
 EXPLAIN
--> Sort: <temporary>.col1
+-> Sort: t1.col1
     -> Stream results
         -> Inner hash join (t2.col1 = t1.col1)  (cost=2.50 rows=4)
             -> Table scan on t2  (cost=0.09 rows=4)
@@ -37,7 +37,7 @@ test.t2	analyze	status	OK
 EXPLAIN FORMAT=tree
 SELECT t1.col1, t2.col1 FROM t1 JOIN t2 ON t1.col1 = t2.col1 ORDER BY t1.col1;
 EXPLAIN
--> Sort: <temporary>.col1
+-> Sort: t1.col1
     -> Stream results
         -> Inner hash join (t1.col1 = t2.col1)  (cost=2.50 rows=4)
             -> Table scan on t2  (cost=0.09 rows=4)
@@ -1506,7 +1506,7 @@ alias1.col_int_key DESC, field1
 LIMIT 2 OFFSET 2;
 EXPLAIN
 -> Limit/Offset: 2/2 row(s)
-    -> Sort: <temporary>.col_int_key DESC, <temporary>.field1
+    -> Sort: alias1.col_int_key DESC, alias1.pk
         -> Filter: (field1 <> 0)
             -> Table scan on <temporary>
                 -> Temporary table with deduplication
@@ -3062,7 +3062,7 @@ t2 LEFT JOIN t3 ON t2.col1 = t3.col1)
 ON t2.col1 = t1.col1
 ORDER BY t1.col1, t2.col1, t3.col1;
 EXPLAIN
--> Sort: <temporary>.col1, <temporary>.col1, <temporary>.col1
+-> Sort: t1.col1, t2.col1, t3.col1
     -> Stream results
         -> Left hash join (t2.col1 = t1.col1)  (cost=1.36 rows=6)
             -> Table scan on t1  (cost=0.55 rows=3)

inikep/percona-server

commit 02f06aa4a5299628339473112949e69b498f74a5

MTR tests added by Percona
PS-8328: MySQL 8 Crash when using group_concat with group by with rollup

https://jira.percona.com/browse/PS-8328

Starting from version 8.0.27 both PS and upstream crashed if query using
both GROUP_CONCAT() aggregate and WITH ROLLUP clause was run.

----------------------------------------------------------------------------

This problem is a regression from the following upstream's commit:

https://github.com/mysql/mysql-server/commit/1407269ba68f5a4f07431f6b6010d8aaf1b81510

commit 1407269ba68f5a4f07431f6b6010d8aaf1b81510
Date:   Thu Apr 15 22:52:53 2021 +0200

Bug #32644631: PRELIMINARY FIXES FOR WL #14419 [wf framebuffer, noclose]
Bug #32802301: REGRESSION: TYPE_CONVERSION_STATUS FIELD_VARSTRING::STORE: ...

It was caused by the fact that after the above commit we have started to
replace references to grouped expression in rollup queries with
Item_rollup_group_item even in cases when they are occurred inside of
top-level aggregate function (which are wrapped in Item_rollup_sum_switcher
themselves). This is not necessary (as grouped expression don't need to be
replaced by NULL in this case) and has broken fragile assumptions in
Item_rollup_sum_switcher and Item_func_group_concat interaction (this code
assumes that both main and cloned versions of Item_func_group_concat
referenced by Item_rollup_sum_switcher use temporary tables with the same
structure, which was not true if expression for one of them was wrapped in
Item_rollup_group_item item).

This patch makes these assumptions valid again by changing the code not
to replace grouped expression in rollup queries with Item_rollup_group_item
objects inside of top-level aggregates again.

Ideally, we should also refactor code in Item_func_group_concat and
Item_rollup_sum_switcher to avoid such fragile assumptions, but such
a change looks more appropriate for the upstream.

----------------------------------------------------------------------------

PS-9222 Test cases for MySQL server crashes on UPDATE after ALTER TABLE

https://perconadev.atlassian.net/browse/PS-9222

Our patches have been reverted in favour of Upstream's fixes in 8.0.38.
The testcases for the scenario have been preserved.

----------------------------------------------------------------------------

Fixed PS-7221: Bug #100402 (crash on select Sys_var_tz::session_value_ptr)

https://jira.percona.com/browse/PS-7221

This is a regression introduced in 6d587a60738842b1f7e5e5b0839acd62db6b9d43.

mysql-test/r/olap.result
@@ -2365,3 +2365,35 @@ EXECUTE ps;
 x
 DROP PREPARE ps;
 DROP TABLE t;
+#
+# PS-8328: MySQL 8 Crash when using group_concat with group by with
+#          rollup.
+#
+CREATE TABLE t1 (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, string TEXT, i INT);
+INSERT INTO t1 (string, i) VALUES ('something', 10), ('something_else', 10), ('something_completely_else', 20);
+SELECT GROUP_CONCAT(string) FROM t1 GROUP BY string WITH ROLLUP;
+GROUP_CONCAT(string)
+something
+something_completely_else
+something_else
+something,something_completely_else,something_else
+SELECT string, GROUP_CONCAT(string) FROM t1 GROUP BY string WITH ROLLUP;
+string	GROUP_CONCAT(string)
+something	something
+something_completely_else	something_completely_else
+something_else	something_else
+NULL	something,something_completely_else,something_else
+SELECT i, GROUP_CONCAT(string) FROM t1 GROUP BY i WITH ROLLUP;
+i	GROUP_CONCAT(string)
+10	something,something_else
+20	something_completely_else
+NULL	something,something_else,something_completely_else
+SELECT i, string, GROUP_CONCAT(string) FROM t1 GROUP BY i, string WITH ROLLUP;
+i	string	GROUP_CONCAT(string)
+10	something	something
+10	something_else	something_else
+10	NULL	something,something_else
+20	something_completely_else	something_completely_else
+20	NULL	something_completely_else
+NULL	NULL	something,something_else,something_completely_else
+DROP TABLE t1;

mysql-test/r/order_by_sortkey.result
@@ -263,3 +263,17 @@ id	select_type	table	partitions	type	possible_keys	key	key_len	ref	rows	filtered
 Warnings:
 Note	1003	/* select#1 */ select `test`.`t1`.`vc` AS `vc` from `test`.`t1` where (`test`.`t1`.`vc` = DATE'2021-02-08') order by `test`.`t1`.`vc` desc
 DROP TABLE t1;
+#
+# BUG: PS-7221 - crash on select Sys_var_tz::session_value_ptr
+#
+CREATE TABLE t1 (
+id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+dt DATETIME NOT NULL
+);
+INSERT INTO t1 SET dt=NOW();
+INSERT INTO t1 (dt) SELECT NOW() FROM t1;
+SELECT id FROM t1 ORDER BY convert_tz(dt,'UTC',@@session.time_zone);
+id
+1
+2
+DROP TABLE t1;

mysql-test/suite/innodb/r/instant_alter_index_prefix.result
@@ -1,0 +1,38 @@
+without prefix field in the index
+CREATE TABLE t1 (c1 TINYTEXT COLLATE ascii_bin NOT NULL, c2 DATETIME(3) NOT NULL, c3 TEXT, UNIQUE KEY (c1(30)));
+INSERT INTO t1 (c1, c2, c3) VALUE ('k1','2021-12-21','something');
+INSERT INTO t1 (c1, c2, c3) VALUE ('k3','2021-12-21','something else');
+ALTER TABLE t1 ADD COLUMN c4 VARCHAR(18) NOT NULL, ALGORITHM=INSTANT;
+SET GLOBAL innodb_log_checkpoint_now = ON;
+SET GLOBAL innodb_page_cleaner_disabled_debug = 1;
+SET GLOBAL innodb_dict_stats_disabled_debug = 1;
+SET GLOBAL innodb_master_thread_disabled_debug = 1;
+SET GLOBAL innodb_checkpoint_disabled = ON;
+UPDATE t1 SET c4 = 'value' WHERE c1 = 'k1';
+# Restart the server and reload the table to see if tables are corrupted.
+# Kill and restart
+# Run a select to confirm that the database started up successfully
+SELECT * FROM t1;
+c1	c2	c3	c4
+k1	2021-12-21 00:00:00.000	something	value
+k3	2021-12-21 00:00:00.000	something else	
+DROP TABLE t1;
+with prefix field in the index
+CREATE TABLE t1 (c1 TINYTEXT COLLATE ascii_bin NOT NULL, c2 DATETIME(3) NOT NULL, c3 TEXT, PRIMARY KEY (c1(30)));
+INSERT INTO t1 (c1, c2, c3) VALUE ('k1','2021-12-21','something');
+INSERT INTO t1 (c1, c2, c3) VALUE ('k3','2021-12-21','something else');
+ALTER TABLE t1 ADD COLUMN c4 VARCHAR(18) NOT NULL, ALGORITHM=INSTANT;
+SET GLOBAL innodb_log_checkpoint_now = ON;
+SET GLOBAL innodb_page_cleaner_disabled_debug = 1;
+SET GLOBAL innodb_dict_stats_disabled_debug = 1;
+SET GLOBAL innodb_master_thread_disabled_debug = 1;
+SET GLOBAL innodb_checkpoint_disabled = ON;
+UPDATE t1 SET c4 = 'value' WHERE c1 = 'k1';
+# Restart the server and reload the table to see if tables are corrupted.
+# Kill and restart
+# Run a select to confirm that the database started up successfully
+SELECT * FROM t1;
+c1	c2	c3	c4
+k1	2021-12-21 00:00:00.000	something	value
+k3	2021-12-21 00:00:00.000	something else	
+DROP TABLE t1;

mysql-test/suite/innodb/r/instant_alter_stored_column_order.result
@@ -1,0 +1,44 @@
+without prefix field in the index
+CREATE TABLE t1 (c1 TINYTEXT COLLATE ascii_bin NOT NULL, c2 DATETIME(3) NOT NULL, c3 TEXT, UNIQUE KEY (c1(30)));
+INSERT INTO t1 (c1, c2, c3) VALUE ('k1','2021-12-21','something');
+INSERT INTO t1 (c1, c2, c3) VALUE ('k3','2021-12-21','something else');
+ALTER TABLE t1
+CHANGE c2 c2 DATETIME(3) NOT NULL AFTER c3,
+ADD COLUMN c4 VARCHAR(18) NOT NULL,
+ALGORITHM=INSTANT;
+SET GLOBAL innodb_log_checkpoint_now = ON;
+SET GLOBAL innodb_page_cleaner_disabled_debug = 1;
+SET GLOBAL innodb_dict_stats_disabled_debug = 1;
+SET GLOBAL innodb_master_thread_disabled_debug = 1;
+SET GLOBAL innodb_checkpoint_disabled = ON;
+UPDATE t1 SET c4 = 'value' WHERE c1 = 'k1';
+# Restart the server and reload the table to see if tables are corrupted.
+# Kill and restart
+# Run a select to confirm that the database started up successfully
+SELECT * FROM t1;
+c1	c3	c2	c4
+k1	something	2021-12-21 00:00:00.000	value
+k3	something else	2021-12-21 00:00:00.000	
+DROP TABLE t1;
+with prefix field in the index
+CREATE TABLE t1 (c1 TINYTEXT COLLATE ascii_bin NOT NULL, c2 DATETIME(3) NOT NULL, c3 TEXT, PRIMARY KEY (c1(30)));
+INSERT INTO t1 (c1, c2, c3) VALUE ('k1','2021-12-21','something');
+INSERT INTO t1 (c1, c2, c3) VALUE ('k3','2021-12-21','something else');
+ALTER TABLE t1
+CHANGE c2 c2 DATETIME(3) NOT NULL AFTER c3,
+ADD COLUMN c4 VARCHAR(18) NOT NULL,
+ALGORITHM=INSTANT;
+SET GLOBAL innodb_log_checkpoint_now = ON;
+SET GLOBAL innodb_page_cleaner_disabled_debug = 1;
+SET GLOBAL innodb_dict_stats_disabled_debug = 1;
+SET GLOBAL innodb_master_thread_disabled_debug = 1;
+SET GLOBAL innodb_checkpoint_disabled = ON;
+UPDATE t1 SET c4 = 'value' WHERE c1 = 'k1';
+# Restart the server and reload the table to see if tables are corrupted.
+# Kill and restart
+# Run a select to confirm that the database started up successfully
+SELECT * FROM t1;
+c1	c3	c2	c4
+k1	something	2021-12-21 00:00:00.000	value
+k3	something else	2021-12-21 00:00:00.000	
+DROP TABLE t1;

mysql-test/suite/innodb/r/percona_create_table_8k.result
@@ -1,0 +1,50 @@
+#
+# PS-9306 : MySQL 8.0.38, 8.4.1 and 9.0.0 crashes on restart if database has 10K tables or more
+#
+CREATE DATABASE test_8k;
+CREATE PROCEDURE create_tables(start_index INT, end_index INT)
+BEGIN
+DECLARE i INT;
+SET i = start_index;
+WHILE i < end_index DO
+SET @table_name = CONCAT('table_', i);
+SET @create_stmt = CONCAT('CREATE TABLE IF NOT EXISTS test_8k.', @table_name, ' (id INT PRIMARY KEY AUTO_INCREMENT, data VARCHAR(255));');
+PREPARE stmt FROM @create_stmt;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+SET i = i + 1;
+END WHILE;
+END|
+CALL create_tables(0*1024,(0+1)*1024);
+CALL create_tables(1*1024,(1+1)*1024);
+CALL create_tables(2*1024,(2+1)*1024);
+CALL create_tables(3*1024,(3+1)*1024);
+CALL create_tables(4*1024,(4+1)*1024);
+CALL create_tables(5*1024,(5+1)*1024);
+CALL create_tables(6*1024,(6+1)*1024);
+CALL create_tables(7*1024,(7+1)*1024);
+# restart
+CREATE PROCEDURE drop_tables(start_index INT, end_index INT)
+BEGIN
+DECLARE i INT;
+SET i = start_index;
+WHILE i < end_index DO
+SET @table_name = CONCAT('table_', i);
+SET @create_stmt = CONCAT('DROP TABLE test_8k.', @table_name, ';');
+PREPARE stmt FROM @create_stmt;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+SET i = i + 1;
+END WHILE;
+END|
+CALL drop_tables(0*1024,(0+1)*1024);
+CALL drop_tables(1*1024,(1+1)*1024);
+CALL drop_tables(2*1024,(2+1)*1024);
+CALL drop_tables(3*1024,(3+1)*1024);
+CALL drop_tables(4*1024,(4+1)*1024);
+CALL drop_tables(5*1024,(5+1)*1024);
+CALL drop_tables(6*1024,(6+1)*1024);
+CALL drop_tables(7*1024,(7+1)*1024);
+DROP DATABASE test_8k;
+DROP PROCEDURE create_tables;
+DROP PROCEDURE drop_tables;

mysql-test/suite/innodb/t/instant_alter_index_prefix.test
@@ -1,0 +1,50 @@
+# PS-9222 Testing if ALGORITHM=INSTANT crashes server
+source include/have_debug.inc;
+
+--echo without prefix field in the index
+CREATE TABLE t1 (c1 TINYTEXT COLLATE ascii_bin NOT NULL, c2 DATETIME(3) NOT NULL, c3 TEXT, UNIQUE KEY (c1(30)));
+INSERT INTO t1 (c1, c2, c3) VALUE ('k1','2021-12-21','something');
+INSERT INTO t1 (c1, c2, c3) VALUE ('k3','2021-12-21','something else');
+
+ALTER TABLE t1 ADD COLUMN c4 VARCHAR(18) NOT NULL, ALGORITHM=INSTANT;
+
+SET GLOBAL innodb_log_checkpoint_now = ON;
+SET GLOBAL innodb_page_cleaner_disabled_debug = 1;
+SET GLOBAL innodb_dict_stats_disabled_debug = 1;
+SET GLOBAL innodb_master_thread_disabled_debug = 1;
+SET GLOBAL innodb_checkpoint_disabled = ON;
+
+UPDATE t1 SET c4 = 'value' WHERE c1 = 'k1';
+
+--echo # Restart the server and reload the table to see if tables are corrupted.
+--source include/kill_and_restart_mysqld.inc
+
+-- echo # Run a select to confirm that the database started up successfully
+SELECT * FROM t1;
+
+# cleanup
+DROP TABLE t1;
+
+--echo with prefix field in the index
+CREATE TABLE t1 (c1 TINYTEXT COLLATE ascii_bin NOT NULL, c2 DATETIME(3) NOT NULL, c3 TEXT, PRIMARY KEY (c1(30)));
+INSERT INTO t1 (c1, c2, c3) VALUE ('k1','2021-12-21','something');
+INSERT INTO t1 (c1, c2, c3) VALUE ('k3','2021-12-21','something else');
+
+ALTER TABLE t1 ADD COLUMN c4 VARCHAR(18) NOT NULL, ALGORITHM=INSTANT;
+
+SET GLOBAL innodb_log_checkpoint_now = ON;
+SET GLOBAL innodb_page_cleaner_disabled_debug = 1;
+SET GLOBAL innodb_dict_stats_disabled_debug = 1;
+SET GLOBAL innodb_master_thread_disabled_debug = 1;
+SET GLOBAL innodb_checkpoint_disabled = ON;
+
+UPDATE t1 SET c4 = 'value' WHERE c1 = 'k1';
+
+--echo # Restart the server and reload the table to see if tables are corrupted.
+--source include/kill_and_restart_mysqld.inc
+
+-- echo # Run a select to confirm that the database started up successfully
+SELECT * FROM t1;
+
+# cleanup
+DROP TABLE t1;

mysql-test/suite/innodb/t/instant_alter_stored_column_order.test
@@ -1,0 +1,56 @@
+# PS-9222 Testing if ALGORITHM=INSTANT crashes server
+source include/have_debug.inc;
+
+--echo without prefix field in the index
+CREATE TABLE t1 (c1 TINYTEXT COLLATE ascii_bin NOT NULL, c2 DATETIME(3) NOT NULL, c3 TEXT, UNIQUE KEY (c1(30)));
+INSERT INTO t1 (c1, c2, c3) VALUE ('k1','2021-12-21','something');
+INSERT INTO t1 (c1, c2, c3) VALUE ('k3','2021-12-21','something else');
+
+ALTER TABLE t1
+CHANGE c2 c2 DATETIME(3) NOT NULL AFTER c3,
+ADD COLUMN c4 VARCHAR(18) NOT NULL,
+ALGORITHM=INSTANT;
+
+SET GLOBAL innodb_log_checkpoint_now = ON;
+SET GLOBAL innodb_page_cleaner_disabled_debug = 1;
+SET GLOBAL innodb_dict_stats_disabled_debug = 1;
+SET GLOBAL innodb_master_thread_disabled_debug = 1;
+SET GLOBAL innodb_checkpoint_disabled = ON;
+
+UPDATE t1 SET c4 = 'value' WHERE c1 = 'k1';
+
+--echo # Restart the server and reload the table to see if tables are corrupted.
+--source include/kill_and_restart_mysqld.inc
+
+-- echo # Run a select to confirm that the database started up successfully
+SELECT * FROM t1;
+
+# cleanup
+DROP TABLE t1;
+
+--echo with prefix field in the index
+CREATE TABLE t1 (c1 TINYTEXT COLLATE ascii_bin NOT NULL, c2 DATETIME(3) NOT NULL, c3 TEXT, PRIMARY KEY (c1(30)));
+INSERT INTO t1 (c1, c2, c3) VALUE ('k1','2021-12-21','something');
+INSERT INTO t1 (c1, c2, c3) VALUE ('k3','2021-12-21','something else');
+
+ALTER TABLE t1
+CHANGE c2 c2 DATETIME(3) NOT NULL AFTER c3,
+ADD COLUMN c4 VARCHAR(18) NOT NULL,
+ALGORITHM=INSTANT;
+
+SET GLOBAL innodb_log_checkpoint_now = ON;
+SET GLOBAL innodb_page_cleaner_disabled_debug = 1;
+SET GLOBAL innodb_dict_stats_disabled_debug = 1;
+SET GLOBAL innodb_master_thread_disabled_debug = 1;
+SET GLOBAL innodb_checkpoint_disabled = ON;
+
+UPDATE t1 SET c4 = 'value' WHERE c1 = 'k1';
+
+--echo # Restart the server and reload the table to see if tables are corrupted.
+--source include/kill_and_restart_mysqld.inc
+
+-- echo # Run a select to confirm that the database started up successfully
+SELECT * FROM t1;
+
+# cleanup
+DROP TABLE t1;

mysql-test/suite/innodb/t/percona_create_table_8k.test
@@ -1,0 +1,89 @@
+--echo #
+--echo # PS-9306 : MySQL 8.0.38, 8.4.1 and 9.0.0 crashes on restart if database has 10K tables or more
+--echo #
+
+CREATE DATABASE test_8k;
+--source include/count_sessions.inc
+--source include/big_test.inc
+
+DELIMITER |;
+CREATE PROCEDURE create_tables(start_index INT, end_index INT)
+BEGIN
+    DECLARE i INT;
+    SET i = start_index;
+    WHILE i < end_index DO
+        SET @table_name = CONCAT('table_', i);
+        SET @create_stmt = CONCAT('CREATE TABLE IF NOT EXISTS test_8k.', @table_name, ' (id INT PRIMARY KEY AUTO_INCREMENT, data VARCHAR(255));');
+        PREPARE stmt FROM @create_stmt;
+        EXECUTE stmt;
+        DEALLOCATE PREPARE stmt;
+        SET i = i + 1;
+    END WHILE;
+END|
+DELIMITER ;|
+
+let $connections=8;
+let $i=0;
+while ($i < $connections) {
+  --connect(C$i,localhost,root,,test)
+  --send_eval CALL create_tables($i*1024,($i+1)*1024)
+  inc $i;
+}
+
+let $i=0;
+while ($i < $connections) {
+  --connection C$i
+  reap;
+  --disconnect C$i
+  --source include/wait_until_disconnected.inc
+  inc $i;
+}
+--connection default
+--source include/wait_until_count_sessions.inc
+
+--source include/restart_mysqld.inc
+
+--assert(`SELECT COUNT(*)=8192 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'test_8k'`)
+
+--source include/count_sessions.inc
+
+DELIMITER |;
+CREATE PROCEDURE drop_tables(start_index INT, end_index INT)
+BEGIN
+    DECLARE i INT;
+    SET i = start_index;
+    WHILE i < end_index DO
+        SET @table_name = CONCAT('table_', i);
+        SET @create_stmt = CONCAT('DROP TABLE test_8k.', @table_name, ';');
+        PREPARE stmt FROM @create_stmt;
+        EXECUTE stmt;
+        DEALLOCATE PREPARE stmt;
+        SET i = i + 1;
+    END WHILE;
+END|
+DELIMITER ;|
+
+let $connections=8;
+let $i=0;
+while ($i < $connections) {
+  --connect(C$i,localhost,root,,test)
+  --send_eval CALL drop_tables($i*1024,($i+1)*1024)
+  inc $i;
+}
+
+let $i=0;
+while ($i < $connections) {
+  --connection C$i
+  reap;
+  --disconnect C$i
+  --source include/wait_until_disconnected.inc
+  inc $i;
+}
+--connection default
+--source include/wait_until_count_sessions.inc
+
+--assert(`SELECT COUNT(*)=0 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'test_8k'`)
+
+DROP DATABASE test_8k;
+DROP PROCEDURE create_tables;
+DROP PROCEDURE drop_tables;

mysql-test/t/olap.test
@@ -1303,3 +1303,16 @@ EXECUTE ps;
 EXECUTE ps;
 DROP PREPARE ps;
 DROP TABLE t;
+
+--echo #
+--echo # PS-8328: MySQL 8 Crash when using group_concat with group by with
+--echo #          rollup.
+--echo #
+
+CREATE TABLE t1 (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, string TEXT, i INT);
+INSERT INTO t1 (string, i) VALUES ('something', 10), ('something_else', 10), ('something_completely_else', 20);
+SELECT GROUP_CONCAT(string) FROM t1 GROUP BY string WITH ROLLUP;
+SELECT string, GROUP_CONCAT(string) FROM t1 GROUP BY string WITH ROLLUP;
+SELECT i, GROUP_CONCAT(string) FROM t1 GROUP BY i WITH ROLLUP;
+SELECT i, string, GROUP_CONCAT(string) FROM t1 GROUP BY i, string WITH ROLLUP;
+DROP TABLE t1;

mysql-test/t/order_by_sortkey.test
@@ -140,3 +140,16 @@ eval $query;
 eval explain $query;
 
 DROP TABLE t1;
+
+--echo #
+--echo # BUG: PS-7221 - crash on select Sys_var_tz::session_value_ptr
+--echo #
+
+CREATE TABLE t1 (
+  id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  dt DATETIME NOT NULL
+);
+INSERT INTO t1 SET dt=NOW();
+INSERT INTO t1 (dt) SELECT NOW() FROM t1;
+SELECT id FROM t1 ORDER BY convert_tz(dt,'UTC',@@session.time_zone);
+DROP TABLE t1;