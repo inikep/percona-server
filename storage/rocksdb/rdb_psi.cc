facebook/mysql-5.6

Remove rdb_sysvars_mutex (#1459)
Summary:
The mutex is used by sysvar update functions and synchronizes updates to a
subset of global system variables. But sysvar update functions are called with
LOCK_global_system_variables server lock taken, making the extra locking in the
SE redundant, thus just remove it.

However it was also partially (writes but not reads) protecting concurrent
accesses to properties_collector_factory variable, thus introduce
synchronization for its class Rdb_tbl_prop_coll_factory by adding an rwlock.

At the same time convert Rdb_tbl_prop_coll_factory pointer fields to const
references, remove redundant explicit from two-argument constructor, add
[[nodiscard]] to touched methods, make the class non-movable. Remove redundant
const from passed by-value arguments.

To enable converting m_ddl_manager field to a const reference, mark
Rdb_ddl_manager::m_rwlock field as mutable, which then enables marking many
logically-const methods as such. Also mark touched methods [[nodiscard]], remove
redundant const from passed by-value arguments, convert pass by const reference
to pass by value for a small object arguments of GL_INDEX_ID type.

Add return value handling for one call site of Rdb_ddl_manager::find_indexes as
pointed out by [[nodiscard]].

Pull Request resolved: https://github.com/facebook/mysql-5.6/pull/1459

Differential Revision: D58491945

fbshipit-source-id: 79f16ff26e8628cd46d1d72f98162069760300cf

storage/rocksdb/rdb_psi.cc
@@ -51,8 +51,8 @@ my_core::PSI_thread_info all_rocksdb_threads[] = {
 my_core::PSI_mutex_key rdb_psi_open_tbls_mutex_key, rdb_signal_bg_psi_mutex_key,
     rdb_signal_drop_idx_psi_mutex_key, rdb_signal_is_psi_mutex_key,
     rdb_signal_mc_psi_mutex_key, rdb_collation_data_mutex_key,
-    rdb_mem_cmp_space_mutex_key, key_mutex_tx_list, rdb_sysvars_psi_mutex_key,
-    rdb_cfm_mutex_key, rdb_sst_commit_key, rdb_block_cache_resize_mutex_key,
+    rdb_mem_cmp_space_mutex_key, key_mutex_tx_list, rdb_cfm_mutex_key,
+    rdb_sst_commit_key, rdb_block_cache_resize_mutex_key,
     rdb_bottom_pri_background_compactions_resize_mutex_key,
     clone_donor_file_metadata_mutex_key, key_rwlock_clone_client_files,
     clone_main_task_remaining_mutex_key, clone_error_mutex_key;
@@ -73,8 +73,6 @@ my_core::PSI_mutex_info all_rocksdb_mutexes[] = {
     {&rdb_mem_cmp_space_mutex_key, "collation space char data init",
      PSI_FLAG_SINGLETON, 0, PSI_DOCUMENT_ME},
     {&key_mutex_tx_list, "tx_list", PSI_FLAG_SINGLETON, 0, PSI_DOCUMENT_ME},
-    {&rdb_sysvars_psi_mutex_key, "setting sysvar", PSI_FLAG_SINGLETON, 0,
-     PSI_DOCUMENT_ME},
     {&rdb_cfm_mutex_key, "column family manager", PSI_FLAG_SINGLETON, 0,
      PSI_DOCUMENT_ME},
     {&rdb_sst_commit_key, "sst commit", PSI_FLAG_SINGLETON, 0, PSI_DOCUMENT_ME},
@@ -92,7 +90,7 @@ my_core::PSI_mutex_info all_rocksdb_mutexes[] = {
 
 my_core::PSI_rwlock_key key_rwlock_collation_exception_list,
     key_rwlock_read_free_rpl_tables, key_rwlock_clone_task_id_set,
-    key_rwlock_clone_active_clones;
+    key_rwlock_clone_active_clones, key_rwlock_tbl_prop_coll_factory_lock;
 
 my_core::PSI_rwlock_info all_rocksdb_rwlocks[] = {
     {&key_rwlock_collation_exception_list, "collation_exception_list",
@@ -104,6 +102,8 @@ my_core::PSI_rwlock_info all_rocksdb_rwlocks[] = {
      0, PSI_DOCUMENT_ME},
     {&key_rwlock_clone_client_files, "clone_client_files", PSI_FLAG_SINGLETON,
      0, PSI_DOCUMENT_ME},
+    {&key_rwlock_tbl_prop_coll_factory_lock, "properties_collector_factory", 0,
+     PSI_FLAG_SINGLETON, PSI_DOCUMENT_ME},
 };
 
 my_core::PSI_cond_key rdb_signal_bg_psi_cond_key,

inikep/percona-server

Upstream commit ID: https://github.com/facebook/mysql-5.6/commit/81fc0cc12992
PS-9395: Merge percona-202404 (https://jira.percona.com/browse/PS-9395)

storage/rocksdb/rdb_psi.cc
@@ -51,8 +51,8 @@ my_core::PSI_thread_info all_rocksdb_threads[] = {
 my_core::PSI_mutex_key rdb_psi_open_tbls_mutex_key, rdb_signal_bg_psi_mutex_key,
     rdb_signal_drop_idx_psi_mutex_key, rdb_signal_is_psi_mutex_key,
     rdb_signal_mc_psi_mutex_key, rdb_collation_data_mutex_key,
-    rdb_mem_cmp_space_mutex_key, key_mutex_tx_list, rdb_sysvars_psi_mutex_key,
-    rdb_cfm_mutex_key, rdb_sst_commit_key, rdb_block_cache_resize_mutex_key,
+    rdb_mem_cmp_space_mutex_key, key_mutex_tx_list, rdb_cfm_mutex_key,
+    rdb_sst_commit_key, rdb_block_cache_resize_mutex_key,
     rdb_bottom_pri_background_compactions_resize_mutex_key;
 
 my_core::PSI_mutex_info all_rocksdb_mutexes[] = {
@@ -73,8 +73,6 @@ my_core::PSI_mutex_info all_rocksdb_mutexes[] = {
     {&rdb_mem_cmp_space_mutex_key, "collation space char data init",
      PSI_FLAG_SINGLETON, 0, PSI_DOCUMENT_ME},
     {&key_mutex_tx_list, "tx_list", PSI_FLAG_SINGLETON, 0, PSI_DOCUMENT_ME},
-    {&rdb_sysvars_psi_mutex_key, "setting sysvar", PSI_FLAG_SINGLETON, 0,
-     PSI_DOCUMENT_ME},
     {&rdb_cfm_mutex_key, "column family manager", PSI_FLAG_SINGLETON, 0,
      PSI_DOCUMENT_ME},
     {&rdb_sst_commit_key, "sst commit", PSI_FLAG_SINGLETON, 0, PSI_DOCUMENT_ME},
@@ -86,13 +84,15 @@ my_core::PSI_mutex_info all_rocksdb_mutexes[] = {
 };
 
 my_core::PSI_rwlock_key key_rwlock_collation_exception_list,
-    key_rwlock_read_free_rpl_tables;
+    key_rwlock_read_free_rpl_tables, key_rwlock_tbl_prop_coll_factory_lock;
 
 my_core::PSI_rwlock_info all_rocksdb_rwlocks[] = {
     {&key_rwlock_collation_exception_list, "collation_exception_list",
      PSI_FLAG_SINGLETON, 0, PSI_DOCUMENT_ME},
     {&key_rwlock_read_free_rpl_tables, "read_free_rpl_tables",
      PSI_FLAG_SINGLETON, 0, PSI_DOCUMENT_ME},
+    {&key_rwlock_tbl_prop_coll_factory_lock, "properties_collector_factory", 0,
+     PSI_FLAG_SINGLETON, PSI_DOCUMENT_ME},
 };
 
 my_core::PSI_cond_key rdb_signal_bg_psi_cond_key,